621e89bad14af94e7bc443273dadabe3a48dc35a
Vic Cherubini <vmc@leftnode.com>
2008-05-15 02:42:53 +0000

    Initial checkin of all server related classes and monitors.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@6 c87a4359-4766-4260-b519-6004f3ce7042

--- Artisan/Server/Monitor.php
+++ Artisan/Server/Monitor.php
@@ -1,18 +1,18 @@
 <?php
 
-class Artisan_Database_Monitor {
+class Artisan_Server_Monitor {
 
 	private static $instance = NULL;
 
-	public static function set(Artisan_Database $db) {
+	public static function set(Artisan_Server $db) {
 		self::$instance = $db;
 	}
 
 	public static function get() {
 		// Example of the Lazy Initialization pattern, default to use Mysql
 		if ( NULL === self::$instance ) {
-			$db = Artisan_Library::load('Database/Mysql', true);
-			if ( true === is_object($db) && $db instanceof Artisan_Database ) {
+			$db = Artisan_Library::load('Server/Curl', true);
+			if ( true === is_object($db) && $db instanceof Artisan_Server ) {
 				self::set($db);
 			}
 		}
