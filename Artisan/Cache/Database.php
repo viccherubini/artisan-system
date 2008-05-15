5722e6142d755c11e74ceb24c5354479c5f3bf68
Vic Cherubini <vmc@leftnode.com>
2008-05-15 02:40:50 +0000

    Checkin of all Cache monitors and classes.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@5 c87a4359-4766-4260-b519-6004f3ce7042

---
+++ Artisan/Cache/Database.php
@@ -0,0 +1,10 @@
+<?php
+
+
+class Artisan_Cache_Database extends Artisan_Cache {
+
+	public function __construct($config = array()) {
+	}
+}
+
+?>
--- Artisan/Cache/Monitor.php
+++ Artisan/Cache/Monitor.php
@@ -1,18 +1,18 @@
 <?php
 
-class Artisan_Database_Monitor {
+class Artisan_Cache_Monitor {
 
 	private static $instance = NULL;
 
-	public static function set(Artisan_Database $db) {
+	public static function set(Artisan_Cache $db) {
 		self::$instance = $db;
 	}
 
 	public static function get() {
 		// Example of the Lazy Initialization pattern, default to use Mysql
 		if ( NULL === self::$instance ) {
-			$db = Artisan_Library::load('Database/Mysql', true);
-			if ( true === is_object($db) && $db instanceof Artisan_Database ) {
+			$db = Artisan_Library::load('Cache/Database', true);
+			if ( true === is_object($db) && $db instanceof Artisan_Cache ) {
 				self::set($db);
 			}
 		}
