27b202599e0b359c71a6fb4726af6c89ce0c4dfa
Vic Cherubini <vmc@leftnode.com>
2008-05-24 11:00:34 +0000

    Testbed changes.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@13 c87a4359-4766-4260-b519-6004f3ce7042

--- index.php
+++ index.php
@@ -3,35 +3,47 @@
 error_reporting(E_ALL);
 
 require_once 'Artisan/Library.php';
+
 Artisan_Library::load('Database');
 Artisan_Library::load('Database/Mysqli');
-Artisan_Library::load('Template');
-Artisan_Library::load('Exception');
 
-//test();
-
-$t = Artisan_Template_Monitor::get();
-try {
-	$t->load('some_template');
-} catch( Artisan_Template_Exception $e ) {
-	echo $e->toString() . '<br />';
-}
+$config = array(
+	'server' => 'localhost',
+	'username' => 'vic',
+	'password' => 'dba89da',
+	'dbname' => 'test'
+);
+	
+Artisan_Database_Monitor::set(new Artisan_Database_Mysqli($config));
+$db = Artisan_Database_Monitor::get();
+$db->connect();
 
 echo '<hr />';
 echo 'Finished processing<br />';
 echo 'Memory: ' . round(( memory_get_peak_usage() / (1024*1024)), 4) . 'MB<br />';
 
-function test() {
-	//Artisan_Database_Monitor::set(new Artisan_Database_Mysqli());
-	$db = Artisan_Database_Monitor::get();
 
-	try {
-		$db->connect();
-	} catch ( Artisan_Database_Exception $e ) {
-		echo $e->toString() . '<br />';
+
+class Test {
+	private $vo;
+
+	public function __construct(VO $a) {
+		$this->vo = $a;
 	}
 
-	$db->query('select * from table were table_id = 54');
-	$db->disconnect();
+	public function g() {
+		echo $this->vo->v();
+	}
+
+}
+
+class VO {
+	private $value = NULL;
+
+	public function __construct($v) {
+		$this->value = $v;
+	}
+
+	public function v() { return $this->value; }
 }
 ?>
