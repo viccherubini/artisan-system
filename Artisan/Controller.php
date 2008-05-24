b1f898f1bf7eba76d67eb0e24ce35ada198f2316
Vic Cherubini <vmc@leftnode.com>
2008-05-24 11:07:16 +0000

    All of these files are the new classes and files, some are empty.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@17 c87a4359-4766-4260-b519-6004f3ce7042

---
+++ Artisan/Controller.php
@@ -0,0 +1,31 @@
+<?php
+
+class Artisan_Controller {
+	public function __construct() {
+
+	}
+
+
+	public function view($view_name) {
+		if ( true === empty($view_name) ) {
+			return NULL;
+		}
+
+		// See if the view exists in the directory
+		$view_file = 'View/' . ucwords( str_replace('_', ' ', $view_name) ) . '.php';
+
+		$view = NULL;
+		if ( true === file_exists($view_file) ) {
+			require_once $view_file;
+
+			$view_class = ucwords( str_replace('_', ' ', $view_name) );
+
+			$view = new $view_class();
+		}
+
+		return $view;
+	}
+
+}
+
+?>
