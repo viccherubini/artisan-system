245951ab79f6a8d7ecbe14d836cb3336701dc2a3
Vic Cherubini <vmc@leftnode.com>
2008-05-24 11:00:25 +0000

    Wrote base class code.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@12 c87a4359-4766-4260-b519-6004f3ce7042

--- Artisan/Database/Mysqli.php
+++ Artisan/Database/Mysqli.php
@@ -2,30 +2,77 @@
 
 
 class Artisan_Database_Mysqli extends Artisan_Database {
-
+	private $_config = array();
+	
+	private $_db_conn = NULL;
+	
+	private $_db_result = NULL;
+	
 	public function __construct($config = array()) {
-		echo 'In mysqli constructor<br />';
-
-		if ( count($config) > 0 ) {
-			echo 'using database: ' . $config['db_name'] . '<br />';
+		if ( true === empty($config) ) {
+			$this->_config = parent::$config;
+		} else {
+			if ( true === is_array($config) && count($config) > 0 ) {
+				$this->_config = $config;
+			}
 		}
 	}
 
-	public function __destruct() {
-		echo 'Mysqli dying<br />';
-	}
-
 	public function connect() {
-		echo 'Mysqli connecting<br />';
+		// Although generally against supressing errors, the @ is to supress a misconnection error
+		// to allow the framework to handle it gracefully
+		$server = $this->_config['server'];
+		$username = $this->_config['username'];
+		$password = $this->_config['password'];
+		$dbname = $this->_config['dbname'];
+		
+		$port = 3306;
+		if ( true === array_key_exists('port', $this->_config) ) {
+			if ( intval($this->_config['port']) > 0 ) {
+				$port = intval($this->_config['port']);
+			}
+		}
+		
+		$this->_db_conn = @new mysqli($server, $username, $_password, $dbname, $port);
+
+		if ( 0 != mysqli_connect_errno() || false === $this->_db_conn ) {
+			$this->_is_connected = false;
+			return false;
+		}
+		
+		$this->_is_connected = true;
+		return $this->_db_conn;
 	}
 
 	public function disconnect() {
-		echo 'Mysqli disconnecting<br />';
+		if ( true === $this->_is_connected ) {
+			$this->_db_conn->close();
+			$this->_db_conn = NULL;
+			$this->_is_connected = false;
+		}
 	}
-
-	public function query($sql) {
-		echo 'Mysqli querying: <strong>' . $sql . '</strong><br />';
+	
+	public function rowCount() { }
+	public function rowsAffected() { }
+	
+	public function query(Artisan_Sql $sql) { }
+	
+	public function fetch() { }
+	public function free() { }
+	
+	public function isConnected() {
+		return $this->_is_connected;
 	}
+	
+	public function escape($string) {
+		return $this->_db_conn->real_escape_string($string);
+	}
+	
+	private function _start() { }
+	private function _cancel() { }
+	private function _end() { }
+	
+	public function queue($query_list) { }
 }
 
 ?>
