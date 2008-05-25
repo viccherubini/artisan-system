742054871412985a0cc574dd8a943a152c29c918
Vic Cherubini <vmc@leftnode.com>
2008-05-25 12:48:44 +0000

    Updated database exception, now simply calls parent constructor.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@19 c87a4359-4766-4260-b519-6004f3ce7042

--- Artisan/Database/Exception.php
+++ Artisan/Database/Exception.php
@@ -2,12 +2,6 @@
 
 
 class Artisan_Database_Exception extends Artisan_Exception {
-
-
-	public function __construct($error_code, $error_message, $class_name = NULL, $function_name = NULL) {
-		$error_message .= ' <strong>IN DATABASE ERROR!</strong>';
-		parent::__construct($error_code, $error_message, $class_name, $function_name);
-	}
 }
 
 ?>
