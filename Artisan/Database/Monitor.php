af08b31065a571836a3b92715b8e54b709082272
Vic Cherubini <vmc@leftnode.com>
2008-05-26 04:48:22 +0000

    Updated get method to take optional parameter  so that the proper type of database can be included. For example, if ->type is set to Mysqli, that database type is loaded rather than the default.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@32 c87a4359-4766-4260-b519-6004f3ce7042

--- Artisan/Database/Monitor.php
+++ Artisan/Database/Monitor.php
@@ -3,15 +3,23 @@
 class Artisan_Database_Monitor {
 
 	private static $instance = NULL;
-
+	
 	public static function set(Artisan_Database $db) {
 		self::$instance = $db;
 	}
 
-	public static function get() {
+	public static function get(Artisan_Config $config = NULL) {
 		// Example of the Lazy Initialization pattern, default to use Mysql
-		if ( NULL === self::$instance ) {
-			$db = Artisan_Library::load('Database/Mysql', true);
+		if ( NULL === self::$instance || false === is_null($config) ) {
+			// Determine if a type is set in the configuration
+			$type = 'Mysql';
+			if ( true === $config->exists('type') ) {
+				$type = ucwords($config->type);
+			}
+		
+			$db = Artisan_Library::load('Database/' . $type, true);
+			
+			
 			if ( true === is_object($db) && $db instanceof Artisan_Database ) {
 				self::set($db);
 			}
