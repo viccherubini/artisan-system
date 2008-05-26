f023ee38c3522d5b33b77ac722f381b7117660be
Vic Cherubini <vmc@leftnode.com>
2008-05-26 04:46:37 +0000

    Removed fake old class methods, added abstract methods setTheme and load, added concrete methods setVariables, getCache, and _parse. Made constructor require  to be type Artisan_Config.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@31 c87a4359-4766-4260-b519-6004f3ce7042

--- Artisan/Template.php
+++ Artisan/Template.php
@@ -2,14 +2,33 @@
 
 
 Artisan_Library::load('Template/Monitor');
+Artisan_Library::load('Template/Exception');
 
 abstract class Artisan_Template {
-
-	public function __construct($config = array()) {
-		echo 'In template constructor<br />';
+	protected static $_config = NULL;
+	
+	public function __construct(Artisan_Config $config = NULL) {
+		$this->_config = $config;
 	}
 
-	abstract public function load($template_name);
+	
+	abstract public function setTheme($theme);
+	abstract public function load($template);
+	
+	
+	public function setVariables() {
+	
+	}
+	
+	
+	public function getCache() {
+	
+	}
+	
+	private function _parse() {
+	
+	
+	}
 }
 
 ?>
