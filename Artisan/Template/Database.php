150ae0e660194492f1e07c752f95492d4786e28e
Vic Cherubini <vmc@leftnode.com>
2008-05-26 04:48:48 +0000

    Initial class creation for loading templates through the database. Constructor needs more thought as to how it is going to work.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@33 c87a4359-4766-4260-b519-6004f3ce7042

--- Artisan/Template/Database.php
+++ Artisan/Template/Database.php
@@ -1,24 +1,50 @@
 <?php
 
-Artisan_Library::load('Template/Exception');
-
 class Artisan_Template_Database extends Artisan_Template {
+	private $_db = NULL;
 
-	public function __construct($config = array()) {
-		echo 'In template::database constructor<br />';
+
+	private $_config = array();
+
+	public function __construct(Artisan_Config $config = NULL) {
+		$this->_config = $config;
+		if ( true === is_null($config) ) {
+			$this->_config = parent::$_config;
+		}
+		
+		if ( false === Artisan_Library::exists('Database') ) {
+			Artisan_Library::load('Database');
+		}
+		
+		// Configuration should specify the database connection
+		// information, if not, use the default database already created.
+		if ( true === is_null($this->_config) || false === is_object($this->_config) ) {
+			// Use the default database connection and configuration created elsewhere
+			// This can be unsafe as at this point, you may not be aware of what the 
+			// last database created was. It is generally suggested you explicitly 
+			// specify the configuration information.
+			$this->_db = Artisan_Database_Monitor::get();
+		} else {
+			// This is the preferred method of creating the 
+			$this->_db = Artisan_Database_Monitor::get($this->_config);
+			$this->_db->setConfig($this->_config);
+		}
+		
+		
 	}
 
 	public function __destruct() {
-		echo 'template::database dying<br />';
+		
 	}
 
-	public function load($template_name) {
-		//throw new Artisan_Template_Exception(1, 'Some error occurred in loading the template', __CLASS__, __FUNCTION__);
-
-		echo 'loading "<strong>' . $template_name . '</strong>" in template::database::load()<br />';
-
-		$db = Artisan_Database_Monitor::get();
-		$db->query('Select * from template where template_id = 1');
+	public function load($tname) {
+		
+		Artisan_Sql_Monitor::set( new Artisan_Sql_Select() );
+		$select = Artisan_Sql_Monitor::get();
+		
+		
+		//$select->from(
+		
 	}
 }
 
