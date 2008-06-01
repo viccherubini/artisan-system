757192b478593f64a669f08d09184721d538b9bb
Vic Cherubini <vmc@leftnode.com>
2008-06-01 15:37:24 +0000

    Added comment to internalize() describing in detail how it works.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@55 c87a4359-4766-4260-b519-6004f3ce7042

--- Artisan/Config.php
+++ Artisan/Config.php
@@ -5,13 +5,29 @@ Artisan_Library::load('Config/Exception');
 
 abstract class Artisan_Config {
 
+	/**
+	 * Set the configuration.
+	 */
 	abstract public function set($cfg);
+	
+	/**
+	 * Load the configuration.
+	 */
 	abstract public function load();
-	
 
-	//abstract public function write();
-	//abstract public function setValue();
-	
+	/**
+	 * Take an array and turn it into into an object like:
+	 * $arr = array(
+	 *     'a' => 'value',
+	 *     'b' => 'value2',
+	 *     'c' => array( 'd' => 'value3' )
+	 * );
+	 * To:
+	 * $object->a has the value 'value', and
+	 * $object->c->d has the value 'value3' in it.
+	 * This way, one can easily internalize a config
+	 * array to access each element easily.
+	 */
 	public static function internalize($root, &$t) {
 		foreach ( $root as $k => $v ) {
 			if ( true === is_array($v) ) {
