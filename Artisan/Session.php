5a77be2a9408a281b03d54e88cbfa3dfba7dbebf
Vic Cherubini <vmc@leftnode.com>
2008-06-01 03:04:29 +0000

    The default constructor now requires the argument to be of type Artisan_Config, included the Session/Exception class as well.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@46 c87a4359-4766-4260-b519-6004f3ce7042

--- Artisan/Session.php
+++ Artisan/Session.php
@@ -2,10 +2,12 @@
 
 
 Artisan_Library::load('Session/Monitor');
+Artisan_Library::load('Session/Exception');
 
 abstract class Artisan_Session {
 
-	public function __construct($config = array()) {
+	public function __construct(Artisan_Config $config) {
+	
 	}
 }
 
