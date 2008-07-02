cae2f02ead1355251bcc9db98adef80d5b2cc6a2
Vic Cherubini <vmc@leftnode.com>
2008-07-02 12:15:41 +0000

    Updated the way load() works, split it out into a private method, _load(). load() now attempts to see if the class being included is a child class (Config_Array, Database_Mysqli, etc) and it's parent class (ie, Config, Database, Template, etc..). If so, the parent class is loaded first, and then the child class is loaded. This means one can do something like Artisan_Library::load('Config/Array') and automatically have Config, Config_Exception and Config_Monitor loaded (the last two are loaded by Config).
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@69 c87a4359-4766-4260-b519-6004f3ce7042

--- Artisan/Library.php
+++ Artisan/Library.php
@@ -7,6 +7,22 @@ class Artisan_Library {
 	private static $object_list = array();
 
 	public static function load($lib_name, $build = false, $system_class = true) {
+		// First, see if this is a concrete class, and if so,
+		// attempt to include it's parent class.
+		if ( false !== strpos($lib_name, '/') ) {
+			$classes = explode('/', $lib_name);
+
+			// Get the first element, which is the parent class
+			if ( false === self::exists($classes[0]) ) {
+				self::_load($classes[0], $build, $system_class);
+			}
+		}
+
+		// Now, load up the child class
+		self::_load($lib_name, $build, $system_class);
+	}
+
+	private static function _load($lib_name, $build = false, $system_class = true ) {
 		$class_prefix = NULL;
 		if ( true === $system_class ) {
 			$class_prefix = ARTISAN_NAME . '_';
@@ -25,9 +41,17 @@ class Artisan_Library {
 			$file = $abs_path . '/' . $lib_name . $ext;
 
 			if ( true === file_exists($file) && true === is_file($file) ) {
-				require_once $file;
-
+				/**
+				 * IMPORTANT!
+				 * Adding the library name before including the library
+				 * means that if a child class is included and the parent
+				 * is automatically included, and the parent includes children
+				 * classes, this method won't attempt to add the parent class
+				 * to the library because it's already been added.
+				 */
 				self::$object_list[] = $lib_name;
+
+				require_once $file;
 
 				if ( true === $build && true === class_exists($class) ) {
 					$instance = new $class;
