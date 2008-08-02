9ce4da4a687e30a2e9f61563b92198776a64f5c7
Vic Cherubini <vmc@leftnode.com>
2008-08-02 12:16:17 +0000

    Made Monitor.php take Auth type.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@75 c87a4359-4766-4260-b519-6004f3ce7042

--- Artisan/Auth/Monitor.php
+++ Artisan/Auth/Monitor.php
@@ -1,18 +1,14 @@
 <?php
 
-
-
-class Artisan_Email_Monitor {
+class Artisan_Auth_Monitor {
 
 	private static $instance = NULL;
 
-	public static function set(Artisan_Email $db) {
-		self::$instance = $db;
+	public static function set(Artisan_Auth $auth) {
+		self::$instance = $auth;
 	}
 
 	public static function get() {
-		// Because the email may require configuration, return nothing
-		// if it doesn't exist.
 		return self::$instance;
 	}
 
