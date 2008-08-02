545f8cdd424264c849649322b1acb9ba9eec3624
Vic Cherubini <vmc@leftnode.com>
2008-08-02 15:38:18 +0000

    Updated both of these to use the new signature of _init() and load(), removed private member variable.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@80 c87a4359-4766-4260-b519-6004f3ce7042

--- Artisan/Config/Array.php
+++ Artisan/Config/Array.php
@@ -1,42 +1,31 @@
 <?php
 
 class Artisan_Config_Array extends Artisan_Config {
-
-	private $_array = NULL;
-	
 	/**
 	 * Default constructor, sets the internal configuration array.
 	 */
 	public function __construct($array) {
-		$this->_array = $array;
-		$this->load();
+		//$this->_array = $array;
+		if ( true === is_array($array) ) {
+			$this->load($array);
+		}
 	}
 	
 	/**
 	 * Default destructor, unsets the internal configuration array.
 	 */
 	public function __destruct() {
-		unset($this->_array);
-	}
-	
-	/**
-	 * Sets the current configuration file if not set in the constructor,
-	 * or if it needs to be reset.
-	 */
-	public function set($cfg) {
-		$this->_array = $cfg;
+		
 	}
 	
 	/**
 	 * Loads the specified XML configuration file. Loads in the XML
 	 * library at runtime if necessary.
 	 */
-	public function load() {
-		if ( true === is_array($this->_array) ) {
-			parent::internalize($this->_array, $this);
+	public function load($source) {
+		if ( true === is_array($source) ) {
+			$this->_init($source);
 		}
-	
-		$this->_array = NULL;
 	}
 }
 
--- Artisan/Config/Xml.php
+++ Artisan/Config/Xml.php
@@ -1,44 +1,31 @@
 <?php
 
 class Artisan_Config_Xml extends Artisan_Config {
-
-	private $_source = NULL;
-	
 	/**
 	 * Default constructor, sets the internal configuration XML.
 	 */
 	public function __construct($source) {
-		$this->_source = $source;
+		
 	}
 	
 	/**
 	 * Default destructor, unsets the internal configuration XML.
 	 */
-	public function __destruct() {
-		unset($this->_source);
-	}
-	
-	/**
-	 * Sets the current configuration file if not set in the constructor,
-	 * or if it needs to be reset.
-	 */
-	public function set($cfg) {
-		$this->_source = $cfg;
-	}
+	public function __destruct() { }
 	
 	/**
 	 * Loads the specified XML configuration file. Loads in the XML
 	 * library at runtime if necessary.
 	 */
-	public function load() {
+	public function load($source) {
 		if ( false === Artisan_Library::exists('Xml') ) {
 			Artisan_Library::load('Xml');
 		}
 		
-		Artisan_Xml::load($this->_source);
+		Artisan_Xml::load($source);
 		$xml = Artisan_Xml::toArray();
 		
-		parent::internalize($xml, $this);
+		$this->_init($xml);
 	}
 }
 
