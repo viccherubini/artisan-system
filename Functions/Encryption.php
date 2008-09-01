d7cc571a58dc3c961c2099c3b07ec652850c599f
Vic Cherubini <vmc@leftnode.com>
2008-09-01 15:59:36 +0000

    Added Doxygen comments to the asfw_encrypt_string() and asfw_decrypt_string() methods.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@135 c87a4359-4766-4260-b519-6004f3ce7042

--- Functions/Encryption.php
+++ Functions/Encryption.php
@@ -1,9 +1,25 @@
 <?php
 
+/**
+ * Encrypts a string given a key with a Rijndael cipher, requires the
+ * mcrypt library to be installed.
+ * @author vmc <vmc@leftnode.com>
+ * @param $key The key to use to encrypt the string.
+ * @param $string The string to encrypt.
+ * @retval string Returns the encrypted string.
+ */
 function asfw_encrypt_string($key, $string) {
 
 }
 
+/**
+ * Decrypts a string given a key with a Rijndael cipher, requires the
+ * mcrypt library to be installed.
+ * @author vmc <vmc@leftnode.com>
+ * @param $key The key to use to decrypt the string.
+ * @param $string The string to decrypt.
+ * @retval string Returns the decrypted string.
+*/
 function asfw_descrypt_string($key, $string) {
 
 }
