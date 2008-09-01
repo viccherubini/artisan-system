29f17e49093c477df201acdbbd84cc45a11750b9
Vic Cherubini <vmc@leftnode.com>
2008-09-01 17:27:01 +0000

    Wrote the body offlush() and updated the default constructor. Renamed  to  since it stores a database objec.t
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@146 c87a4359-4766-4260-b519-6004f3ce7042

--- Log/Database.php
+++ Log/Database.php
@@ -2,45 +2,42 @@
 
 
 class Artisan_Log_Database extends Artisan_Log {
+	private $DB = NULL;
 	
-	private $_db = NULL;
-	
-	public function __construct(Artisan_Config $config = NULL) {
-		if ( true === is_object($config) ) {
-			/*
-			$type = @$config->type;
-			$type = ucwords($type);
-			
-			$class = 'Artisan_Database_' . $type;
-			$this->_db = new $class($config);
-			*/
-			
-			// Want to do something like $db = new Artisan_Database($config) and it
-			// knows to use the Mysqli based on the type
-			//$this->_db = new Artisan_Database($config);
-		} else {
-			$db = Artisan_Database_Monitor::get();
-			if ( true === is_object($db) && $db instanceof Artisan_Database ) {
-				$this->_db = $db;			
+	/**
+	 * Constructor for the Artisan_Log class to save logs to the database.
+	 * @author vmc <vmc@leftnode.com>
+	 * @throws Artisan_Log_Exception If the database object passed into the class can not be connected to.
+	 * @retval Object The new Artisan_Log_Database object.
+	 */
+	public function __construct(Artisan_Database &$db) {
+		if ( false === $db->isConnected() ) {
+			// Try to connect to the database
+			try {
+				$db->connect();
+			} catch ( Artisan_Database_Exception $e ) {
+				throw new Artisan_Log_Exception(ARTISAN_ERROR_CORE, $e->getMessage(), __CLASS__, __FUNCTION__);
 			}
 		}
 		
-		
-		// If $_db is still null, throw an exception
-		if ( true === is_null($this->_db) ) {
-			throw new Artisan_Log_Exception(
-				ARTISAN_ERROR_CORE,
-				'Failed to initiliaze Log::Database class because no suitable database connection could be found.',
-				__CLASS__,
-				__FUNCTION__
-			);
-		}
+		$this->DB = &$db;
 	}
 	
-	public function flush() {
-		//pprint_r(parent::$_log);
-	}
 
+	public function flush() {
+		if ( count($this->_log) > 0 ) {
+			foreach ( $this->_log as $log ) {
+				// See if this type can be inserted into the database as per the flush levels
+				if ( true === in_array($log['log_type'], $this->_flush_level_list) ) {
+					try {
+						$this->DB->insert->into($this->_db_table, array_keys($log))->values($log)->query();
+					} catch ( Artisan_Sql_Exception $e ) {
+						// Can't do anything with $e
+					}
+				}
+			}
+		}
+	}
 }
 
 ?>
