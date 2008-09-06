10c1ebb66d138664cce16b0a30b21f2a5aa645b7
Vic Cherubini <vmc@leftnode.com>
2008-09-06 05:04:07 +0000

    Updated the constructor about checking if the database is currently connectied, added Doxygen comments.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@161 c87a4359-4766-4260-b519-6004f3ce7042

--- Log/Database.php
+++ Log/Database.php
@@ -2,35 +2,30 @@
 
 
 class Artisan_Log_Database extends Artisan_Log {
+	///< Database instance passed into the class. Assumes the database already has a connection.
 	private $DB = NULL;
+	
+	
+	const TABLE_LOG = 'artisan_log';
 	
 	/**
 	 * Constructor for the Artisan_Log class to save logs to the database.
 	 * @author vmc <vmc@leftnode.com>
-	 * @throws Artisan_Log_Exception If the database object passed into the class can not be connected to.
-	 * @retval Object The new Artisan_Log_Database object.
+	 * @retval object The new Artisan_Log_Database object.
 	 */
 	public function __construct(Artisan_Database &$db) {
-		if ( false === $db->isConnected() ) {
-			// Try to connect to the database
-			try {
-				$db->connect();
-			} catch ( Artisan_Database_Exception $e ) {
-				throw new Artisan_Log_Exception(ARTISAN_ERROR_CORE, $e->getMessage(), __CLASS__, __FUNCTION__);
-			}
-		}
-		
+		// We can only assume the database has a current connection
+		// as we don't want to attempt to connect.
 		$this->DB = &$db;
 	}
 	
-
 	public function flush() {
 		if ( count($this->_log) > 0 ) {
 			foreach ( $this->_log as $log ) {
 				// See if this type can be inserted into the database as per the flush levels
 				if ( true === in_array($log['log_type'], $this->_flush_level_list) ) {
 					try {
-						$this->DB->insert->into($this->_db_table, array_keys($log))->values($log)->query();
+						$this->DB->insert->into(self::TABLE_LOG, array_keys($log))->values($log)->query();
 					} catch ( Artisan_Sql_Exception $e ) {
 						// Can't do anything with $e
 					}
