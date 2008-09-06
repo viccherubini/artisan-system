4bcd9ea806cbb5409d24b074d42003e6ee47bc3b
Vic Cherubini <vmc@leftnode.com>
2008-09-06 04:12:06 +0000

    Updated the get() method to not use lazy initialization because we can't be sure where anything is coming from
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@157 c87a4359-4766-4260-b519-6004f3ce7042

--- Template/Monitor.php
+++ Template/Monitor.php
@@ -1,7 +1,6 @@
 <?php
 
 class Artisan_Template_Monitor {
-
 	private static $instance = NULL;
 
 	public static function set(Artisan_Template $t) {
@@ -9,17 +8,8 @@ class Artisan_Template_Monitor {
 	}
 
 	public static function get() {
-		// Example of the Lazy Initialization pattern, default to use Database 
-		if ( NULL === self::$instance ) {
-			$t = Artisan_Library::load('Template/Database', true);
-			if ( true === is_object($t) && $t instanceof Artisan_Template ) {
-				self::set($t);
-			}
-		}
-
 		return self::$instance;
 	}
-
 }
 
 ?>
