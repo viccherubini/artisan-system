6c2d2d0aabc637bf454de1181528e09bbfa022ca
Vic Cherubini <vmc@leftnode.com>
2008-09-07 05:32:48 +0000

    Wrote the majority of this class to authenticate a person against a database.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@171 c87a4359-4766-4260-b519-6004f3ce7042

--- Auth/Database.php
+++ Auth/Database.php
@@ -1,16 +1,65 @@
 <?php
 
 class Artisan_Auth_Database extends Artisan_Auth {
-
-	public function __construct() {
+	private $DB = NULL;
 	
+	///< The name of the table that holds user data, this is also defined in Artisan/User/Database.php!
+	const TABLE_USER = 'artisan_user';
 	
+	public function __construct(Artisan_Database &$db) {
+		// We can only assume the database has a current connection
+		// as we don't want to attempt to connect.
+		$this->DB = &$db;
 	}
 	
-	public function __destruct() {
-	
-	}
 
+	public function authenticate($validation_hook = NULL) {
+		$authenticated = false;
+		
+		// See if a user has been set, if not, throw an exception
+		if ( false === is_object($this->USER) ) {
+			throw new Artisan_Auth_Exception(ARTISAN_ERROR_CORE, 'Failed to authenticate, the user object has not been set.', __CLASS__, __FUNCTION__);
+		}
+		
+		// Get the username and password from the user object
+		
+		// Always assume the password is hashed already as it shouldn't be stored
+		// unhashed in the Artisan_User class.
+		$user_name = $this->USER->getUserName();
+		$user_password = $this->USER->getUserPassword();
+		
+		$this->DB->select
+			->from(self::TABLE_USER, 'au')
+			->where(array('user_name' => $user_name, 'user_password' => $user_password))
+			->query();
+			
+		$row_count = $this->DB->select->numRows();
+		
+		// First, ensure at least one row was found.
+		if ( 0 === $row_count ) {
+			throw new Artisan_Auth_Exception(ARTISAN_WARNING, 'Failed to authenticate, no matching records found.', __CLASS__, __FUNCTION__);
+		}
+		
+		// Next, ensure not more than one row was found.
+		if ( $row_count > 1 ) {
+			throw new Artisan_Auth_Exception(ARTISAN_WARNING, 'Failed to authenticate, more than one matching record found. ' . $row_count . ' records found.', __CLASS__, __FUNCTION__);
+		}
+		
+		$user_data = $this->DB->select->fetch();
+		
+		// Ok, we're certain only one matching record was found.
+		// See if there is a hook to call on the data returned
+		if ( false === empty($validation_hook) ) {
+			$hook = new ReflectionFunction($validation_hook);
+			$authenticated = $hook->invoke($user_data);
+		} else {
+			// No post validation hooks, made it this far, everything is authorized
+			// and ready to go.
+			$authenticated = true;
+		}
+		
+		return $authenticated;
+	}
 }
 
 ?>
