478722dc6e12c35e13f09ddc0eb62082fcf86aff
Vic Cherubini <vmc@leftnode.com>
2008-09-09 00:30:47 +0000

    Added Doxygen comments
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@174 c87a4359-4766-4260-b519-6004f3ce7042

--- Functions/System.php
+++ Functions/System.php
@@ -1,9 +1,14 @@
 <?php
 
+/**
+ * Returns the peak memory in MB during script execution.
+ * @author vmc <vmc@leftnode.com>
+ * @retval float The peak memory used during script execution in megabytes.
+ */
 function asfw_peak_memory() {
 	$one_mb = 1024*1024;
 	$memory = memory_get_peak_usage() / $one_mb;
-	$memory = round($memory, 4) . 'MB';
+	$memory = round($memory, 4);
 	return $memory;
 }
 
