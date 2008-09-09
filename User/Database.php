4e8495f8fa985a983589625b4cd3b17be45d1663
Vic Cherubini <vmc@leftnode.com>
2008-09-09 00:33:04 +0000

    Added the database connector for manipulating users in a database.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@176 c87a4359-4766-4260-b519-6004f3ce7042

--- User/Database.php
+++ User/Database.php
@@ -14,6 +14,62 @@ class Artisan_User_Database extends Artisan_User {
 	public function __construct(Artisan_Database &$db) {
 		$this->DB = &$db;
 	}
+	
+	public function write() {
+	
+	}
+	
+	public function load($user_id) {
+		try {
+			$this->_load($user_id);
+		} catch ( Artisan_User_Exception $e ) {
+			throw $e;
+		}
+	}
+	
+	protected function _load($user_id) {
+		$user_id = intval($user_id);
+		
+		if ( $user_id < 1 ) {
+			throw new Artisan_User_Exception(ARTISAN_WARNING, 'The user ID specified must be numeric and greater than 0', __CLASS__, __FUNCTION__);
+		}
+		
+		$this->DB->select
+			->from(self::TABLE_USER, asfw_create_table_alias(self::TABLE_USER))
+			->where(array('user_id' => $user_id))
+			->query();
+		$row_count = $this->DB->select->numRows();
+		
+		if ( 1 !== $row_count ) {
+			throw new Artisan_User_Exception(ARTISAN_WARNING, 'No user found with ID ' . $user_id . '.', __CLASS__, __FUNCTION__);
+		}
+		
+		// Now that we have a user, load up their data
+		$user_data = $this->DB->select->fetch();
+		$user_data = new Artisan_VO($user_data);
+		
+		$this->setUserId($user_id);
+		$this->setUserName($user_data->user_name);
+		$this->setUserPassword($user_data->user_password);
+		$this->setUserPasswordSalt($user_data->user_password_salt);
+		$this->setUserEmailAddress($user_data->user_email_address);
+		$this->setUserFirstname($user_data->user_firstname);
+		$this->setUserMiddlename($user_data->user_middlename);
+		$this->setUserLastname($user_data->user_lastname);
+		$this->setUserStatus($user_data->user_status);
+	}
+	
+	protected function _insert() {
+	
+	}
+	
+	protected function _update() {
+	
+	}
+	
+	protected function _makeRecord() {
+	
+	}
 }
 
 ?>
