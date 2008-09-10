001cd8aaab0c7ed9133db0aba1fab0d55ecac65d
Vic Cherubini <vmc@leftnode.com>
2008-09-10 10:50:35 +0000

    Added Doxygen comments to _init() and __toString()
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@182 c87a4359-4766-4260-b519-6004f3ce7042

--- Config.php
+++ Config.php
@@ -1,8 +1,12 @@
 <?php
 
-Artisan_Library::load('Config/Monitor');
+//Artisan_Library::load('Config/Monitor');
 Artisan_Library::load('Config/Exception');
 
+/**
+ * This class holds all configuration data that can come from different sources.
+ * @author vmc <vmc@leftnode.com>
+ */
 abstract class Artisan_Config {
 	/**
 	 * Load the configuration.
@@ -11,6 +15,7 @@ abstract class Artisan_Config {
 
 	/**
 	 * Take an array and turn it into into an object like:
+	 * @author vmc <vmc@leftnode.com>
 	 * @code
 	 * $arr = array(
 	 *     'a' => 'value',
@@ -23,6 +28,7 @@ abstract class Artisan_Config {
 	 * $object->c->d has the value 'value3' in it.
 	 * This way, one can easily internalize a config
 	 * array to access each element easily.
+	 * @retval NULL Returns nothing.
 	 */
 	public function _init($root) {
 		foreach ( $root as $k => $v ) {
@@ -35,6 +41,11 @@ abstract class Artisan_Config {
 		}
 	}
 	
+	/**
+	 * Echos out the configuration as a print_r() with a pre wrapped around it.
+	 * @author vmc <vmc@leftnode.com>
+	 * @retval string Returns a string representation of $this.
+	 */
 	public function __toString() {
 		return asfw_print_r($this, true);		
 	}
