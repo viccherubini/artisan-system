9dcf7ef1559db9331dfe7dd4beb82a7359350831
Vic Cherubini <vmc@leftnode.com>
2008-09-10 10:52:56 +0000

    Added more Doxygen comments to arrayToJson() and jsonToArray()
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@186 c87a4359-4766-4260-b519-6004f3ce7042

--- Json.php
+++ Json.php
@@ -1,13 +1,14 @@
 <?php
 
 /**
- * This class handles encoding and decoding of JSON. It requires PHP 5.2 or greater
- * to operate.
+ * This class handles encoding and decoding of JSON. It requires PHP 5.2 or greater to operate.
+ * @author vmc <vmc@leftnode.com>
  */
 class Artisan_Json {
 	/**
 	 * Convert an associative array to a JSON "string"
 	 * @author vmc <vmc@leftnode.com>
+	 * @retval string The JSON string.
 	 */
 	public static function arrayToJson($data) {
 		if ( true === function_exists('json_encode') ) {
@@ -21,6 +22,8 @@ class Artisan_Json {
 	
 	/**
 	 * Convert a JSON "string" to an associative array.
+	 * @author vmc <vmc@leftnode.com>
+	 * @retval array The array of JSON data.
 	 */
 	public static function jsonToArray($data) {
 		if ( true === function_exists('json_decode') ) {
