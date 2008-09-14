7c14af4a33cb9a25c283ec23a1547263da7a975f
Vic Cherubini <vmc@leftnode.com>
2008-09-14 05:18:57 +0000

    Wrote the _makeRecord() method and updated all of the methods to take out the word User as it is redundant.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@190 c87a4359-4766-4260-b519-6004f3ce7042

--- User.php
+++ User.php
@@ -13,16 +13,16 @@ abstract class Artisan_User {
 
 	protected $_user_status;
 
-	public function setUserId($user_id) {
+	public function setId($user_id) {
 		$user_id = intval($user_id);
 		$this->_user_id = ( $user_id > 0 ? $user_id : 0 );
 	}
 
-	public function setUserName($user_name) {
+	public function setName($user_name) {
 		$this->_user_name = trim($user_name);
 	}
 
-	public function setUserEmailAddress($email_address) {
+	public function setEmailAddress($email_address) {
 		// Validate the email address
 		$email_address = trim($email_address);
 		//if ( true === Artisan_Validate::validateEmailAddress($email_address) ) {
@@ -30,64 +30,79 @@ abstract class Artisan_User {
 		//}
 	}
 
-	public function setUserFirstname($firstname) {
+	public function setFirstname($firstname) {
 		$this->_user_firstname = trim($firstname);
 	}
 
-	public function setUserMiddlename($middlename) {
+	public function setMiddlename($middlename) {
 		$this->_user_middlename = trim($middlename);
 	}
 
-	public function setUserLastname($lastname) {
+	public function setLastname($lastname) {
 		$this->_user_lastname = trim($lastname);
 	}
 
-	public function setUserPassword($password) {
+	public function setPassword($password) {
 		$this->_user_password = trim($password);
 	}
 	
-	public function setUserPasswordSalt($salt) {
+	public function setPasswordSalt($salt) {
 		$this->_user_password_salt = trim($salt);
 	}
 	
-	public function setUserStatus($status) {
+	public function setStatus($status) {
 		$status = intval($status);
 		$this->_user_status = $status;
 	}
 
-	public function getUserId() {
+	public function getId() {
 		return $this->_user_id;
 	}
 
-	public function getUserName() {
+	public function getName() {
 		return $this->_user_name;
 	}
 
-	public function getUserPassword() {
+	public function getPassword() {
 		return $this->_user_password;
 	}
 
-	public function getUserEmailAddress() {
+	public function getEmailAddress() {
 		return $this->_user_email_address;
 	}
 
-	public function getUserFirstname() {
+	public function getFirstname() {
 		return $this->_user_firstname;
 	}
 
-	public function getUserMiddlename() {
+	public function getMiddlename() {
 		return $this->_user_middlename;
 	}
 
-	public function getUserLastname() {
+	public function getLastname() {
 		return $this->_user_lastname;
+	}
+
+	protected function _makeRecord() {
+		$user_data = array(
+			'user_id' => $this->getId(),
+			'user_name' => $this->getName(),
+			'user_password' => $this->getPassword(),
+			'user_password_salt' => $this->getPasswordSalt(),
+			'user_email_address' => $this->getEmailAddress(),
+			'user_firstname' => $this->getFirstname(),
+			'user_middlename' => $this->getMiddleName(),
+			'user_lastname' => $this->getLastname(),
+			'user_status' => $user_name
+		);
+		
+		return new Artisan_VO($user_data);
 	}
 
 	abstract public function write();
 	abstract protected function _load($user_id);
 	abstract protected function _insert();
 	abstract protected function _update();
-	abstract protected function _makeRecord();
 }
 
 ?>
