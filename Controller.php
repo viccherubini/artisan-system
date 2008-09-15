1e40741a2ee6326de24c6d2c2f91b40458d66db8
Vic Cherubini <vmc@leftnode.com>
2008-09-15 03:31:24 +0000

    Added a class level Doxygen comment, updated the constructor to create the instance of the  variable which is an instance of the controller plugin class.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@195 c87a4359-4766-4260-b519-6004f3ce7042

--- Controller.php
+++ Controller.php
@@ -1,23 +1,23 @@
 <?php
 
+
+
 /**
- * Handles the Model-View Controller design pattern. 
+ * Handles the Model-View Controller design pattern. The Plugin architecture allows
+ * one to easily push a class (Aritsan or not) into the controller so that children
+ * classes can easily take advantage of them.
  * @author vmc <vmc@leftnode.com>
  */
 class Artisan_Controller {
+	protected $P = NULL;
+	
 	public function __construct() {
-
+		$this->P = &Artisan_Controller_Plugin::get();
 	}
 
 	public function __destruct() {
 	
 	}
-
-	public function view($view_name) {
-	
-	
-	}
-
 }
 
 ?>
