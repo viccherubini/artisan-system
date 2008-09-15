d62ea171f10709b9ca58827d24ce00f761c316fd
Vic Cherubini <vmc@leftnode.com>
2008-09-15 03:34:47 +0000

    Added all of the abstract class signatures and wrote the isConnected(), _setIsConnected(), and _unsetIsConnected() methods.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@200 c87a4359-4766-4260-b519-6004f3ce7042

--- Server.php
+++ Server.php
@@ -4,8 +4,42 @@ Artisan_Library::load('Server/Monitor');
 Artisan_Library::load('Server/Exception');
 
 abstract class Artisan_Server {
+	protected $_server_address = NULL;
+	protected $_server_port = 0;
+	protected $_is_connected = false;
+	
 
-	public function __construct($config = array()) {
+	public function __construct(Artisan_Config &$C) {
+		$this->_unsetIsConnected();
+		
+		$this->_server_address = $C->server_address;
+		$this->_server_port = $C->server_port;
+		unset($C);
+	}
+	
+	abstract public function connect();
+	
+	abstract public function close();
+	
+	abstract public function setHeader($name, $value);
+	
+	abstract public function setHeaderList($header_list);
+	
+	abstract public function send();
+	
+	abstract public function error();
+	
+		
+	public function isConnected() {
+		return $this->_is_connected;
+	}
+	
+	protected function _setIsConnected() {
+		$this->_is_connected = true;
+	}
+	
+	protected function _unsetIsConnected() {
+		$this->_is_connected = false;
 	}
 }
 
