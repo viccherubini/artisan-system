3d5acc20adde414c6c00b19148296ba1932e800e
Vic Cherubini <vmc@leftnode.com>
2008-09-15 03:36:18 +0000

    Wrote the entire class to connect to another server via Curl.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@204 c87a4359-4766-4260-b519-6004f3ce7042

--- Server/Curl.php
+++ Server/Curl.php
@@ -3,8 +3,80 @@
 Artisan_Library::load('Server/Exception');
 
 class Artisan_Server_Curl extends Artisan_Server {
+	private $_server = NULL;
+	
+	
 
-	public function __construct($config = array()) {
+	public function __destruct() {
+		if ( true === $this->isConnected() ) {
+			$this->close();
+		}
+	}
+	
+	public function connect() {
+		$server = trim($this->_server_address);
+		if ( true === empty($server) ) {
+			throw new Artisan_Server_Exception(ARTISAN_WARNING, 'No server was specified to connect to.', __CLASS__, __FUNCTION__);
+		}
+		
+		$this->_server = curl_init($server);
+		
+		if ( false === $this->_server ) {
+			throw new Artisan_Server_Exception(ARTISAN_WARNING, 'Failed to connect to specified server: ' . $server, __CLASS__, __FUNCTION__);
+		}
+		
+		$this->_setIsConnected();
+		return true;
+	}
+	
+	public function close() {
+		if ( true === $this->isConnected() ) {
+			curl_close($this->_server);
+			$this->_unsetIsConnected();
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public function setHeader($name, $value) {
+		if ( true === $this->isConnected() ) {
+			return curl_setopt($this->_server, $name, $value);
+		}
+		
+		return false;
+	}
+	
+	public function setHeaderList($header_list) {
+		if ( true === $this->isConnected() ) {
+			if ( true === is_array($header_list) ) {
+				return curl_setopt_array($this->_server, $header_list);
+			}
+		}
+			
+		return false;
+	}
+	
+	public function send() {
+		if ( true === $this->isConnected() ) {
+			$response = curl_exec($this->_server);
+			return $response;
+		}
+	}
+	
+	public function error() {
+		if ( true === $this->isConnected() ) {
+			$error_no = curl_errno($this->_server);
+			$error_str = curl_error($this->_server);
+		
+			if ( $error_no == 0 && true === empty($error_str) ) {
+				return true;
+			} else {
+				return $error_str;
+			}
+		}
+		
+		return false;
 	}
 }
 
