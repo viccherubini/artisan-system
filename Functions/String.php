87d7828756737822f43d689691cce85e227cf91d
Vic Cherubini <vmc@leftnode.com>
2008-09-16 11:55:47 +0000

    Added method asfw_rename_controller() which takes a string like this_is_a_string and turns it into This_Is_A_String for converting url names to class names.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@205 c87a4359-4766-4260-b519-6004f3ce7042

--- Functions/String.php
+++ Functions/String.php
@@ -12,4 +12,17 @@ function asfw_first_letter($word) {
 	return $w;
 }
 
+function asfw_rename_controller($controller) {
+	$controller = strtolower($controller);
+	
+	// Strip out non-ascii characters
+	$controller = preg_replace('/[^a-z_0-9]/i', NULL, $controller);
+	$controller = str_replace('_', ' ', $controller);
+	$controller = ucwords($controller);
+	$controller = str_replace(' ', '_', $controller);
+	
+	return $controller;
+}
+
+
 ?>
