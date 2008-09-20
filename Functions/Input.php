a08e9614ebcf1d6013bd6b3851edf5ed6ada6107
Vic Cherubini <vmc@leftnode.com>
2008-09-20 20:48:48 +0000

    Updated asfw_get_ipv4() to really return an IP address if the HTTP_X_FORWARDED_FOR is used.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@217 c87a4359-4766-4260-b519-6004f3ce7042

--- Functions/Input.php
+++ Functions/Input.php
@@ -1,10 +1,18 @@
 <?php
 
 function asfw_get_ipv4() {
-	$ip_address = $_SERVER['REMOTE_ADDR'];
-	// Validate the IP address
-
-	return $ip_address;
+	$ip = NULL;
+	if ( true === isset($_SERVER) ) {
+		if ( true === asfw_exists('HTTP_X_FORWARDED_FOR', $_SERVER) ) {
+			$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+		} elseif ( true === asfw_exists('HTTP_CLIENT_IP', $_SERVER)) {
+			$ip = $_SERVER['HTTP_CLIENT_IP'];
+		} else {
+			$ip = $_SERVER['REMOTE_ADDR'];
+		}
+	}
+	
+	return $ip;
 }
 
 ?>
