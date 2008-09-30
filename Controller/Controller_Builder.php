79627218bd28e5284c4298b3cca0118ff8c83bcf
Vic Cherubini <vmc@leftnode.com>
2008-09-30 10:28:42 +0000

    Added methods setConfig(), setTranslationTable(), registerPlugin(), execute(), and _build()
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@231 c87a4359-4766-4260-b519-6004f3ce7042

--- Controller/Controller_Builder.php
+++ Controller/Controller_Builder.php
@@ -7,7 +7,7 @@ class Artisan_Controller_Builder {
 	private static $CTT = NULL;
 	private static $CONFIG = NULL;
 
-	
+	private static $CONTROLLER = NULL;
 
 
 	private function __construct() {
@@ -23,7 +23,7 @@ class Artisan_Controller_Builder {
 			self::$INST = new self;
 		}
 		
-		
+		$this->_build();
 		
 		return self::$INST;
 	}
@@ -34,6 +34,23 @@ class Artisan_Controller_Builder {
 
 	public function setTranslationTable(Artisan_VO &$CTT) {
 		$this->CTT = &$CTT;
+	}
+	
+	
+	public function registerPlugin(&$PLUGIN, $name) {
+		if ( true === empty($name) ) {
+			throw new Artisan_Controller_Exception(ARTISAN_CORE_ERROR, 'Failed to register plugin, $name is NULL.', __CLASS__, __FUNCTION__);
+		}
+		
+		Artisan_Controller_Plugin::get()->register($PLUGIN, $name);
+	}
+	
+	public function execute() {
+	
+	}
+	
+	private function _build() {
+		self::$CONTROLLER = new Artisan_Controller(self::$CONFIG);
 	}
 }
 
