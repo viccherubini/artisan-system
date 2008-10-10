fe771997acf5d1632b64f4c500d5e0033b0d5277
Vic Cherubini <vmc@leftnode.com>
2008-10-10 11:33:00 +0000

    Wrote all of the body of Artisan_Sql_Delete_Mysqli to build off Artisan_Sql_Delete.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@243 c87a4359-4766-4260-b519-6004f3ce7042

--- Sql/Mysqli/Delete.php
+++ Sql/Mysqli/Delete.php
@@ -1,5 +1,26 @@
 <?php
 
+//asfw_print_r(get_declared_classes());
+
 class Artisan_Sql_Delete_Mysqli extends Artisan_Sql_Delete {
+
+	private $CONN = NULL;
+	private $RESULT = NULL;
+
+	public function __construct($CONN) {
+		if ( true === $CONN instanceof mysqli ) {
+			$this->CONN = $CONN;
+		}
+	}
+
+	public function __destruct() { }
+
+	public function build() {
+
+	}
+
+	public function query() {
+
+	}
 
 }
