1f047736e0a2d8c8e2795c93b89e6d763457c84e
Vic Cherubini <vmc@leftnode.com>
2008-10-10 11:31:17 +0000

    Removed many of the methods that are no longer needed.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@240 c87a4359-4766-4260-b519-6004f3ce7042

--- Sql.php
+++ Sql.php
@@ -4,6 +4,8 @@ Artisan_Library::load('Sql/Monitor');
 Artisan_Library::load('Sql/Exception');
 
 class Artisan_Sql {
+	///< The list of fields to use in the WHERE clause.
+	protected $_where_field_list = array();
 	
 	const SQL_AND = 'AND';
 	const SQL_OR = 'OR';
@@ -17,89 +19,25 @@ class Artisan_Sql {
 	public function __destruct() {
 
 	}
-
 	
-	
-	
-	
-	
-	/*
-	protected static function createFieldList($table, $fields, $alias = NULL) {
-		$field_list = array();
-		
-		if ( true === is_array($fields) ) {
-			$field_list = array_map("aliasize", $fields, array_fill(0, count($fields), $alias));
+	public function where($where_fields) {
+		if ( true === asfw_is_assoc($where_fields) ) {
+			$this->setWhereFieldList(asfw_sanitize_field_list($where_fields));
+		} else {
+			$this->setWhereFieldList(array());
 		}
 		
-		return $field_list;
+		return $this;
 	}
 	
-	protected static function _where($table, $fields, $field_data, $type = 'AND', $alias = NULL) {
-		$type = strtoupper($type);
-		
-		if ( 'OR' != $type && 'AND' != $type ) {
-			$type = 'AND';
+	public function setWhereFieldList($where_field_list) {
+		if ( true === is_array($where_field_list) ) {
+			$this->_where_field_list = $where_field_list;
 		}
-		
-		$type = str_pad($type, strlen($type) + 2, ' ', STR_PAD_BOTH);
-		
-		if ( false === is_array($fields) || count($fields) < 1 ) {
-			$fields = array($fields);
-		}
-		
-		$field_list = self::createFieldList($table, $fields, $alias);
-	
-		$i=0;
-		$fl = NULL;
-		
-		$safe_data_function = '$this->safeData';
-		if ( true === Artisan_Library::exists('Database') ) {
-			$db = Artisan_Database_Monitor::get();
-			
-			if ( true === is_object($db) ) {
-				$safe_data_function = '$db->safeData';
-			}
-		}
-		
-		foreach ( $field_list as $field ) {
-			$data = "''";
-			if ( true === array_key_exists($i, $field_data) ) {
-				$data = $field_data[$i];
-			}
-			
-			$field = str_replace('?', "'" . $data . "'", $field);
-			$fl .= ( 0 === $i ? NULL : $type . ' ' ) . $field;
-			$i++;
-		}
-		
-		$sql = ' WHERE ' . $fl;
-		
-		return $sql;
+		return true;
 	}
 	
-	protected static function _query($sql) {
-		if ( true === Artisan_Library::exists('Database') ) {
-			$db = Artisan_Database_Monitor::get();
-			
-			if ( true === is_object($db) ) {
-				$result = $db->querydb($sql);
-				return $result;
-			}
-		}
-		
-		return NULL;
-	}
-	*/
 	
-	/**
-	 * If there is no database connection, use this function to
-	 * make a value safe for the database!
-	 */
-	/*
-	private function safeData($str) {
-		return addslashes($str);
-	}
-	*/
 }
 
 ?>
