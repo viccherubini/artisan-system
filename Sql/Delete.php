16615a04fca87c35bd7a9a39aa56c53ba394d7a4
Vic Cherubini <vmc@leftnode.com>
2008-10-10 11:32:23 +0000

    Wrote the entirity of the base abstract class.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@242 c87a4359-4766-4260-b519-6004f3ce7042

--- Sql/Delete.php
+++ Sql/Delete.php
@@ -1,8 +1,47 @@
 <?php
 
-class Artisan_Sql_Delete extends Artisan_Sql {
-
-
+abstract class Artisan_Sql_Delete extends Artisan_Sql {
+	///< The actual SQL query in string form.
+	protected $_sql = NULL;
+	
+	///< The main table the query is selecting FROM.
+	protected $_from_table = NULL;
+	
+	///< The alias of the table the query is selecting FROM.
+	protected $_from_table_alias = NULL;
+	
+	///< The list of fields to use in the WHERE clause.
+	//protected $_where_field_list = array();
+		
+	
+	public function __construct() {
+		$this->_sql = NULL;
+	}
+	
+	public function __destruct() {
+		unset($this->_sql, $this->_from_table, $this->_from_table_list, $this->_where_field_list);
+	}
+	
+	
+	public function from($table) {
+	
+	}
+	
+	/*
+	public function where($where_fields) {
+		if ( true === asfw_is_assoc($where_fields) ) {
+			$this->setWhereFieldList(asfw_sanitize_field_list($where_fields));
+		}
+		
+		return $this;
+	}
+	*/
+	
+	
+	
+	
+	abstract public function build();
+	abstract public function query();
 }
 
 ?>
