42ea81ca237cbf2069cf7d1d98d58607f872f58b
Vic Cherubini <vmc@leftnode.com>
2008-10-11 13:09:53 +0000

    Wrote build() method to build the query, wrote query() method to execute the new query and escape() to properly escape the values.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@251 c87a4359-4766-4260-b519-6004f3ce7042

--- Sql/Mysqli/Delete.php
+++ Sql/Mysqli/Delete.php
@@ -1,7 +1,5 @@
 <?php
 
-//asfw_print_r(get_declared_classes());
-
 class Artisan_Sql_Delete_Mysqli extends Artisan_Sql_Delete {
 
 	private $CONN = NULL;
@@ -16,11 +14,30 @@ class Artisan_Sql_Delete_Mysqli extends Artisan_Sql_Delete {
 	public function __destruct() { }
 
 	public function build() {
-
+		$where_sql = NULL;
+		if ( count($this->_where_field_list) > 0 ) {
+			$where_list = array();
+			foreach ( $this->_where_field_list as $field => $value ) {
+				$where_list[] = $field . " = '" . $this->escape($value) . "'";
+			}
+			$where_sql = " WHERE " . implode(" AND ", $where_list);
+		}
+		
+		$limit_sql = NULL;
+		if ( $this->_limit > 0 ) {
+			$limit_sql = " LIMIT " . $this->_limit;
+		}
+		
+		
+		$delete_start = "DELETE FROM `" . $this->_from_table . "` ";
+		$this->_sql = $delete_start . $where_sql . $limit_sql;
 	}
 
 	public function query() {
-
+		$this->build();
 	}
-
+	
+	public function escape($value) {
+		return $this->CONN->real_escape_string($value);
+	}
 }
