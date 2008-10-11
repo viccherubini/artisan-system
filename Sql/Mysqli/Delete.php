f654cdd8758731b92b68134e8b40d74630e67397
Vic Cherubini <vmc@leftnode.com>
2008-10-11 13:45:18 +0000

    Wrote the bulk of the query() method and added methods autoOptimize(), optimize() and affectedRows().
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@256 c87a4359-4766-4260-b519-6004f3ce7042

--- Sql/Mysqli/Delete.php
+++ Sql/Mysqli/Delete.php
@@ -1,7 +1,8 @@
 <?php
 
 class Artisan_Sql_Delete_Mysqli extends Artisan_Sql_Delete {
-
+	private $_auto_optimize = false;
+	
 	private $CONN = NULL;
 	private $RESULT = NULL;
 
@@ -14,6 +15,11 @@ class Artisan_Sql_Delete_Mysqli extends Artisan_Sql_Delete {
 	public function __destruct() { }
 
 	public function build() {
+		// Ensure that the connection actually exists before building the query.
+		if ( false === $this->CONN instanceof mysqli ) {
+			throw new Artisan_Sql_Exception(ARTISAN_WARNING, 'The current connection to the database does not exist, no query can be built.', __CLASS__, __METHOD__);
+		}
+		
 		$where_sql = NULL;
 		if ( count($this->_where_field_list) > 0 ) {
 			$where_list = array();
@@ -35,9 +41,37 @@ class Artisan_Sql_Delete_Mysqli extends Artisan_Sql_Delete {
 
 	public function query() {
 		$this->build();
+		
+		if ( true === empty($this->_sql) ) {
+			throw new Artisan_Sql_Exception(ARTISAN_WARNING, 'The DELETE query is empty, it can not be executed.', __CLASS__, __FUNCTION__);
+		}
+		
+		$result = $this->CONN->query($this->_sql);
+		
+		if ( false === $result ) {
+			throw new Artisan_Sql_Exception(ARTISAN_WARNING, 'Failed to execute the DELETE query: ' . $this->_sql, __CLASS__, __FUNCTION__);
+		}
+		
+		if ( true === $this->_auto_optimize ) {
+			$this->optimize();
+		}
+		
+		return $this;
 	}
 	
 	public function escape($value) {
 		return $this->CONN->real_escape_string($value);
+	}
+	
+	public function autoOptimize() {
+		$this->_auto_optimize = true;
+	}
+	
+	public function optimize() {
+		$this->CONN->query('OPTIMIZE TABLE `' . $this->_from_table);
+	}
+	
+	public function affectedRows() {
+		return $this->CONN->affected_rows;
 	}
 }
