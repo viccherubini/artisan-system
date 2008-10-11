f1d58b4e446468a9392993fc58e6b0eb761c6366
Vic Cherubini <vmc@leftnode.com>
2008-10-11 13:09:20 +0000

    Added more Doxygen comments to limit() and reset(), added methods limit(), reset() and __toString(), and removed the ability to specify a table alias.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@250 c87a4359-4766-4260-b519-6004f3ce7042

--- Sql/Delete.php
+++ Sql/Delete.php
@@ -7,41 +7,69 @@ abstract class Artisan_Sql_Delete extends Artisan_Sql {
 	///< The main table the query is selecting FROM.
 	protected $_from_table = NULL;
 	
-	///< The alias of the table the query is selecting FROM.
-	protected $_from_table_alias = NULL;
-	
-	///< The list of fields to use in the WHERE clause.
-	//protected $_where_field_list = array();
-		
+	///< The number of rows that should be deleted
+	protected $_limit = 0;
 	
 	public function __construct() {
 		$this->_sql = NULL;
 	}
 	
 	public function __destruct() {
-		unset($this->_sql, $this->_from_table, $this->_from_table_list, $this->_where_field_list);
+		unset($this->_sql, $this->_from_table, $this->_from_table_list);
 	}
 	
-	
+	/**
+	 * Specifies what table to delete from.
+	 * @author vmc <vmc@leftnode.com>
+	 * @param $table The name of the table
+	 * @throw Artisan_Sql_Exception If the table name is empty.
+	 * @retval Object Returns itself for chainability.
+	 */
 	public function from($table) {
-	
-	}
-	
-	/*
-	public function where($where_fields) {
-		if ( true === asfw_is_assoc($where_fields) ) {
-			$this->setWhereFieldList(asfw_sanitize_field_list($where_fields));
+		$table = trim($table);
+		if ( true === empty($table) ) {
+			throw new Artisan_Sql_Exception(ARTISAN_WARNING, 'Failed to create valid SQL class, the table name is empty.', __CLASS__, __FUNCTION__);
 		}
+		
+		//$this->setFromTable($table);
+		$this->_from_table = $table;
 		
 		return $this;
 	}
-	*/
 	
+	/**
+	 * Sets the limit for the number of rows that should be deleted.
+	 * @author vmc <vmc@leftnode.com>
+	 * @param $limit Positive integer limit.
+	 * @retval Object Returns itself for chainability.
+	 */
+	public function limit($limit) {
+		$limit = abs(intval($limit));
+		$this->_limit = $limit;
+		
+		return $this;
+	}
 	
+	/**
+	 * Resets all of the internal variables that make up a SQL query.
+	 * @author vmc <vmc@leftnode.com>
+	 * @retval boolean Returns true.
+	 */
+	public function reset() {
+		$this->setFromTable(NULL);
+		$this->setFromTableAlias(NULL);
+		$this->setWhereFieldList(array());
+		
+		return true;
+	}
 	
+	public function __toString() {
+		return $this->_sql;
+	}
 	
 	abstract public function build();
 	abstract public function query();
+	abstract public function escape($value);
 }
 
 ?>
