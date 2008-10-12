df5fb79ac69d2c252cff8de54e09b34566fc1f3e
Vic Cherubini <vmc@leftnode.com>
2008-10-12 15:08:20 +0000

    Added Doxygen comments to all of the methods.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@263 c87a4359-4766-4260-b519-6004f3ce7042

--- Sql/Mysqli/Insert.php
+++ Sql/Mysqli/Insert.php
@@ -1,18 +1,40 @@
 <?php
 
+/**
+ * Inserts a query against a MySQL database after being built by chainable commands.
+ * @author vmc <vmc@leftnode.com>
+ */
 class Artisan_Sql_Insert_Mysqli extends Artisan_Sql_Insert {
+	///< The connection object to the database, assumes the database is already connected.
 	private $CONN = NULL;
 	
+	/**
+	 * Builds a new object.
+	 * @author vmc <vmc@leftnode.com>
+	 * @param $CONN A database connection object of type mysqli. Assumes the database is already connected.
+	 * @retval Object A new instance of this class.
+	 */
 	public function __construct($CONN) {
 		if ( true === $CONN instanceof mysqli ) {
 			$this->CONN = $CONN;
 		}
 	}
 	
+	/**
+	 * Destroys this object.
+	 * @author vmc <vmc@leftnode.com>
+	 * @retval NULL Returns nothing, destroys the object.
+	 */
 	public function __destruct() {
-	
 	}
 	
+	/**
+	 * Builds the query to execute. If any of the values are one of the specified values
+	 * defined in the switch below, they will be converted to their special value equivalent.
+	 * @author vmc <vmc@leftnode.com>
+	 * @throw Artisan_Sql_Exception If the connection is not the correct type (class mysqli).
+	 * @retval boolean Returns true.
+	 */
 	public function build() {
 		// Ensure that the connection actually exists before building the query.
 		if ( false === $this->CONN instanceof mysqli ) {
@@ -52,9 +74,17 @@ class Artisan_Sql_Insert_Mysqli extends Artisan_Sql_Insert {
 		$insert_value_sql = " VALUES (" . implode(", ", $value_list) . ") ";
 
 		$this->_sql = $insert_sql . $insert_field_sql . $insert_value_sql;
+		
+		return true;
 	}
 	
-	
+	/**
+	 * Executes the query against the database.
+	 * @author vmc <vmc@leftnode.com>
+	 * @throw Artisan_Sql_Exception If the SQL built is empty.
+	 * @throw Artisan_Sql_Exception If the query fails to execute, the error string from the database is thrown.
+	 * @retval Object Returns an instance of itself for chaining.
+	 */
 	public function query() {
 		// Assume $this->_sql has not been built, so build it. If it
 		// has been built, it'll simply be overwritten.
@@ -73,10 +103,20 @@ class Artisan_Sql_Insert_Mysqli extends Artisan_Sql_Insert {
 		return $this;
 	}
 	
+	/**
+	 * Returns the number of rows inserted.
+	 * @author vmc <vmc@leftnode.com>
+	 * @retval int Returns the number of rows affected by the INSERT.
+	 */
 	public function affectedRows() {
 		return $this->CONN->affected_rows;
 	}
-		
+	
+	/**
+	 * Escapes a string based on the character set of the current connection.
+	 * @author vmc <vmc@leftnode.com>
+	 * @retval string Returns a context escaped string.
+	 */
 	public function escape($value) {
 		return $this->CONN->real_escape_string($value);
 	}
