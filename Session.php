22f28240dc077bfb7c4d3a3d71fadc13769489db
Vic Cherubini <vmc@leftnode.com>
2008-10-26 13:33:24 +0000

    Wrote most of the Artisan_Session class, made it not a singleton, needs to be converted back into a singleton.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@272 c87a4359-4766-4260-b519-6004f3ce7042

--- Session.php
+++ Session.php
@@ -1,10 +1,198 @@
 <?php
 
 Artisan_Library::load('Session/Exception');
+Artisan_Library::load('Session/Interface');
 
-abstract class Artisan_Session {
+class Artisan_Session {
+	///< Since this is a singleton, the instance of this class.
+	//private static $INST = NULL;
 
-	public function __construct(Artisan_Config &$config) {
+	///< The save_handler instance for how this class should be built.
+	private $SH;
+
+
+	private $_session_name;
+	private $_session_id;
+
+	/**
+	 * Private constructor because this class is a singleton.
+	 * @author vmc <vmc@leftnode.com>
+	 * @retval NULL Returns nothing.
+	 */
+	public function __construct() { }
+	
+	/**
+	 * Private clone method because this class is a singleton.
+	 * @author vmc <vmc@leftnode.com>
+	 * @retval NULL Returns nothing.
+	 */
+	//private function __clone() { }
+	
+	/**
+	 * Public destructor.
+	 * @author vmc <vmc@leftnode.com>
+	 * @retval boolean Returns true.
+	 */
+	public function __destruct() {
+		
+	}
+	
+	/**
+	 * The method to return the instance of this class.
+	 * This method provides a singleton interface to this class.
+	 * @author vmc <vmc@leftnode.com>
+	 * @retval Object Returns an instance of the Artisan_Session class.
+	 */
+	//public static function &get() {
+	//	if ( true === is_null(self::$INST) ) {
+	//		self::$INST = new self;
+	//	}
+	//
+	//	return self::$INST;
+	//}
+	
+	/**
+	 * Sets the configuration to this class so the save_handler can be loaded.
+	 * @author vmc <vmc@leftnode.com>
+	 * @param $C The configuration object to collect data from, passed by reference.
+	 * @throw Artisan_Session_Exception If the key save_handler is not present in the configuration.
+	 * @throw Artisan_Session_Exception If the save_handler fails to set.
+	 * @retval boolean Returns true.
+	 */
+	public function setConfig(Artisan_Config &$C) {
+		if ( false === asfw_exists('save_handler', $C) ) {
+			throw new Artisan_Session_Exception(ARTISAN_WARNING, 'save_handler is not present in the configuration.', __CLASS__, __FUNCTION__);
+		}
+		
+		try {
+			$this->setSaveHandler($C->save_handler);
+		} catch ( Artisan_Session_Exception $e ) {
+			throw $e;
+		}
+		
+		return true;
+	}
+	
+	/**
+	 * Sets the save_handler object.
+	 * @author vmc <vmc@leftnode.com>
+	 * @param $S The save_handler object to save session data to.
+	 * @throw Artisan_Session_Exception If the parameter is not of type Artisan_Session.
+	 * @retval boolean Returns true.
+	 */
+	public function setSaveHandler(&$S) {
+		if ( false === is_object($S) ) {
+			throw new Artisan_Session_Exception(ARTISAN_WARNING, 'The save_handler sent in $S is not an object.', __CLASS__, __FUNCTION__);
+		}
+	
+		if ( false === in_array('Artisan_Session_Interface', class_implements($S)) ) {
+			throw new Artisan_Session_Exception(ARTISAN_WARNING, 'The save_handler instance passed is an object, but does not implement Artisan_Session_Interface.', __CLASS__, __FUNCTION__);
+		}
+		
+		$this->SH = &$S;
+		
+		return true;
+	}
+	
+	public function start($session_name = NULL) {
+		if ( false === empty($session_name) ) {
+			$match_alpha_num = preg_match('/^[a-zA-Z0-9]+$/', $session_name);
+			if ( false === $match_alpha_num || true === is_numeric($session_name) ) {
+				$session_name = NULL;
+			}
+		}
+
+		if ( false === empty($session_name) ) {
+			$this->_session_name = $session_name;
+			session_name($session_name);
+		}
+
+		// Set up all of the save handlers
+		if ( false === is_object($this->SH) ) {
+			throw new Artisan_Session_Exception(ARTISAN_WARNING, 'The save_handler is not yet configured, please run setSaveHandler() first.', __CLASS__, __FUNCTION__);
+		}
+
+		// Let PHP know we're using our own save handlers
+		ini_set('session.save_handler', 'user');
+
+		/**
+		 * IMPORTANT! This must be here: the write() method of the save_handler
+		 * is called at the end of the program execution, meaning the destructor
+		 * for class instances are called. The database class instance's destructor
+		 * will be called by the time write() is run, which the destructor disconnects
+		 * from the database and kills the configuration data. By putting this here,
+		 * the shutdown function session_write_close() will be called before that
+		 * occurs, allowing the data to be written properly.
+		*/
+		register_shutdown_function("session_write_close");
+
+		// $this->SH is guaranteed to have these methods because it has
+		// to implement the interface Artisan_Session_Interface
+		session_set_save_handler(
+			array($this->SH, 'open'),
+			array($this->SH, 'close'),
+			array($this->SH, 'read'),
+			array($this->SH, 'write'),
+			array($this->SH, 'destroy'),
+			array($this->SH, 'gc')
+		);
+
+		$started = session_start();
+		$this->_session_id = session_id();
+
+		return $started;
+	}
+	
+	public function destroy() {
+		session_destroy();
+
+		/**
+		 * Really delete the session! Actually, this removes the entire session
+		 * and ensures all data is deleted. The second value of setcookie() is
+		 * intentionally not NULL because IE messes up if you send a NULL cookie.
+		 * Also, this is set at the beginning of time! :)
+		 */
+		if ( true === asfw_exists($this->_session_name, $_COOKIE) ) {
+			setcookie($this->_session_name, '', 1, '/');
+		}
+		
+		return true;
+	}	
+		
+	public function add($name, $value) {
+		$_SESSION[$name] = $value;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*
+	public function remove($name) {
 	
 	}
+	
+	public function exists($name) {
+	
+	}
+	
+	public function registered($name) {
+		return array_key_exists($name, $_SESSION);
+	}
+	*/
+	
+	
 }
