935a1473318e72b89ee52f8fbf13c5154d393701
Vic Cherubini <vmc@leftnode.com>
2008-10-26 13:34:25 +0000

    Most of the Artisan_Session_Database save_handler was written and uses the new Artisan_Session_Interface interface for compatability. The Artisan_Session class allows you to use your own save_handlers as long as they implement the Artisan_Session_Interface interface.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@273 c87a4359-4766-4260-b519-6004f3ce7042

--- Session/Database.php
+++ Session/Database.php
@@ -1,7 +1,50 @@
 <?php
 
-class Artisan_Session_Database extends Artisan_Session {
+class Artisan_Session_Database implements Artisan_Session_Interface {
+	///< Database instance passed into the class. Assumes the database already has a connection.
+	private $DB = NULL;
+	
+	public function __construct(Artisan_Database &$DB) {
+		$this->DB = &$DB;
+	}
+	
+	public function __destruct() {
 
-	public function __construct(Artisan_Database &$db) {
+	}
+	
+	public function open() {
+		return true;
+	}
+	
+	public function close() {
+		return true;
+	}
+	
+	public function read($session_id) {
+		//echo 'reading ' . $session_id . '<br />';
+	}
+	
+	public function write($session_id, $session_data) {
+		// Because not all databases support REPLACE, this is
+		// intentionally inefficient to support the most database types
+		$count = $this->DB->select->from('artisan_session')->where(array('session_id' => $session_id))->query()->numRows();
+		if ( 1 === $count ) {
+			// Update
+		} else {
+			$this->DB->insert
+				->into('artisan_session', array('session_id', 'session_expiration_time', 'session_data'))
+				->values($session_id, time(), $session_data)
+				->query();
+		}
+		
+		return true;
+	}
+	
+	public function destroy($session_id) {
+		//echo 'destroying ' . $session_id . '<br />';
+	}
+	
+	public function gc($life) {
+		//echo 'garbage collecting older than ' . (time()-$life) . ' sessions.';
 	}
 }
