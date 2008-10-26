0316f922d5412a97a8e073ec636942d5d31dfeb3
Vic Cherubini <vmc@leftnode.com>
2008-10-26 14:44:33 +0000

    Updated the destroy(), gc(), write() and close() to have actual bodies.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@278 c87a4359-4766-4260-b519-6004f3ce7042

--- Session/Database.php
+++ Session/Database.php
@@ -4,8 +4,12 @@ class Artisan_Session_Database implements Artisan_Session_Interface {
 	///< Database instance passed into the class. Assumes the database already has a connection.
 	private $DB = NULL;
 	
+	
+	private $_max_lifetime = 1440;
+	
 	public function __construct(Artisan_Database &$DB) {
 		$this->DB = &$DB;
+		$this->_max_lifetime = intval(get_cfg_var("session.gc_maxlifetime"));
 	}
 	
 	public function __destruct() {
@@ -17,6 +21,7 @@ class Artisan_Session_Database implements Artisan_Session_Interface {
 	}
 	
 	public function close() {
+		$this->gc($this->_max_lifetime); 
 		return true;
 	}
 	
@@ -25,26 +30,66 @@ class Artisan_Session_Database implements Artisan_Session_Interface {
 	}
 	
 	public function write($session_id, $session_data) {
+		$error = false;
+		
 		// Because not all databases support REPLACE, this is
 		// intentionally inefficient to support the most database types
-		$count = $this->DB->select->from('artisan_session')->where(array('session_id' => $session_id))->query()->numRows();
+		try {
+			$count = $this->DB->select
+				->from('artisan_session')
+				->where(array('session_id' => $session_id))
+				->query()
+				->numRows();
+		} catch ( Artisan_Database_Exception $e ) {
+			// Do nothing, assume count is 0 and insert into the database
+			$count = 0;
+			$error = true;
+		}
+		
+		
 		if ( 1 === $count ) {
 			// Update
 		} else {
-			$this->DB->insert
-				->into('artisan_session', array('session_id', 'session_expiration_time', 'session_data'))
-				->values($session_id, time(), $session_data)
-				->query();
+			try {
+				$this->DB->insert
+					->into('artisan_session', array('session_id', 'session_expiration_time', 'session_data'))
+					->values($session_id, time(), $session_data)
+					->query();
+			} catch ( Artisan_Database_Exception $e ) {
+				// Do nothing
+				$error = true;
+			}
 		}
 		
-		return true;
+		return !$error;
 	}
 	
 	public function destroy($session_id) {
-		//echo 'destroying ' . $session_id . '<br />';
+		$error = false;
+		try {
+			$this->DB->delete
+				->from('artisan_session')
+				->where(array('session_id' => $session_id))
+				->query();
+		} catch ( Artisan_Database_Exception $e ) {
+			$error = true;
+		}
+		
+		return !$error;
 	}
 	
 	public function gc($life) {
-		//echo 'garbage collecting older than ' . (time()-$life) . ' sessions.';
+		$error = false;
+		try {
+			$del_time = time() - $life;
+			$this->DB->delete
+				->from('artisan_session')
+				->where(array('session_expiration_time >' => $del_time));
+			echo $this->DB->delete;
+		} catch ( Artisan_Database_Exception $e ) {
+			$error = true;
+		}
+
+		return !$error;
 	}
 }
