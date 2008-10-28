6e74f6843b7c417897ee8b6181e081c379de3234
Vic Cherubini <vmc@leftnode.com>
2008-10-28 10:43:36 +0000

    Added the method buildWhereClause() to build the WHERE clause with different operators.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@282 c87a4359-4766-4260-b519-6004f3ce7042

--- Sql.php
+++ Sql.php
@@ -31,12 +31,57 @@ class Artisan_Sql {
 		return $this;
 	}
 	
+	/**
+	 * Because several SQL classes use a WHERE clause, this class
+	 * sets the list of fields in the WHERE clause. It bubbles up to
+	 * the rest of the classes.
+	 * @author vmc <vmc@leftnode.com>
+	 * @param $where_field_list An array of fields in the WHERE clause.
+	 * @retval boolean Returns true.
+	 */
 	public function setWhereFieldList($where_field_list) {
 		if ( true === is_array($where_field_list) ) {
 			$this->_where_field_list = $where_field_list;
 		}
 		return true;
 	}
+	
+	public function buildWhereClause($where_field_list) {
+		$where_sql = NULL;
+		if ( count($where_field_list) > 0 ) {
+			$logical_op_list = array('=', '<', '>', '<=', '>=', '<>', '!=', 'LIKE');
+			$where_list = array();
+			foreach ( $where_field_list as $field => $value ) {
+				// See if field has an operator at the end of it, if so, use that
+				// rather than the equals operator, otherwise, use equals by default.
+				$field = trim($field);
+				
+				if ( false !== strpos($field, ' ') ) {
+					// There's a space, see if an operator exists
+					$op_list = explode(' ', $field);
+					if ( count($op_list) != 2 ) {
+						$field = $op_list[0];
+						$operator = $op_list[count($op_list)-1];
+					} else {
+						$field = $op_list[0];
+						$operator = $op_list[1];
+					}
+						
+					if ( false === in_array($operator, $logical_op_list) ) {
+						$operator = '=';
+					}
+				} else {
+					$operator = '=';
+				}
+				
+				$where_list[] = $field . ' ' . $operator . " '" . $this->escape($value) . "'";
+			}
+			$where_sql = " WHERE " . implode(" AND ", $where_list);
+		}
+		
+		return $where_sql;
+	}
+	
 	
 	/**
 	 * Converts the SQL to a string to echo out.
