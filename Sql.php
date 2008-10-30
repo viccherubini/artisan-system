efca55dfcdf910d875f9326521ddab88a5aa29c6
Vic Cherubini <vmc@leftnode.com>
2008-10-30 10:15:37 +0000

    Updated buildWhereClause() by removing the body and updating it to just implode the where_field_list array.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@293 c87a4359-4766-4260-b519-6004f3ce7042

--- Sql.php
+++ Sql.php
@@ -21,16 +21,98 @@ class Artisan_Sql {
 
 	}
 	
-	public function where($where_fields) {
-		if ( true === asfw_is_assoc($where_fields) ) {
-			$this->setWhereFieldList(asfw_sanitize_field_list($where_fields));
-		} else {
-			$this->setWhereFieldList(array());
+	public function where() {		
+		$argv = func_get_args();
+		$argc = count($argv);
+		
+		$where_item = NULL;
+		
+		if ( 1 === $argc ) {
+			$where_item = $argv[0];
+		} elseif ( $argc > 1 ) {
+			$field_op = trim($argv[0]);
+			
+			// Make an array of values to replace
+			$fv_list = array_splice($argv, 1, $argc, array());
+			
+			$fvl_len = count($fv_list);
+			for ( $i=0; $i<$fvl_len; $i++ ) {
+				$fv = $this->escape($fv_list[$i]);
+				if ( false === is_numeric($fv) ) {
+					$fv = "'" . $fv . "'";
+				}
+				
+				$fv_list[$i] = $fv;
+			}
+			
+			// If there are less ?'s than elements in the array,
+			// something is wrong, so ignore everything
+			$qm_count = substr_count($field_op, '?');
+			
+			if ( $qm_count >= $fvl_len ) {
+				$qm_list = array_fill(0, $qm_count, '/[?]/i');
+				//$where_item = str_replace($qm_list, $fv_list, $field_op);
+				
+				//for ( $i=0; $i<$qm_count; $i++ ) {
+				//	$field_op = str_replace('?', $fv_list[$i], $field_op);
+				//}
+				
+				/*
+				// To make this robust, we're going to loop through each character of 
+				// $field_op and if we come upon a ?, replace it with the top of 
+				// $fv_list queue.
+				$fo_len = strlen($field_op);
+				for ( $i=0; $i<$fo_len; $i++ ) {
+					if ( '?' == $field_op[$i] ) {
+						$replace = array_shift($fv_list);
+						$field_op[$i] = $replace;
+					}
+				}
+				*/
+				$where_item = $field_op;
+				
+			}
+			//echo $where_item;
+		}		
+		/*
+		switch ( $argc ) {
+			case 1: {
+				// The first element is just a plain "field op value" query, no replacement
+				
+				break;
+			}
+			
+			case 2: {
+				// The first element is "field op ?" and the second value is the replacement
+				$field_op = trim($argv[0]);
+				$field_value = $argv[1];
+				
+				// If field_value is numeric, don't include the quotes, otherwise, use them
+				$field_value = $this->escape($field_value);
+				if ( false === is_numeric($field_value) ) {
+					$field_value = "'" . $field_value . "'";
+				}
+				
+				$where_item = str_replace('?', $field_value, $field_op);
+				
+				break;
+			}
+			
+			default: {
+				$where_item = NULL;
+				break;
+			}
+		} 
+		
+		if ( false === empty($where_item) ) {
+			$this->_where_field_list[] = $where_item;
 		}
+		*/
 		
 		return $this;
 	}
-	
+
+
 	/**
 	 * Because several SQL classes use a WHERE clause, this class
 	 * sets the list of fields in the WHERE clause. It bubbles up to
@@ -46,37 +128,11 @@ class Artisan_Sql {
 		return true;
 	}
 	
-	public function buildWhereClause($where_field_list) {
+	public function buildWhereClause() {
 		$where_sql = NULL;
-		if ( count($where_field_list) > 0 ) {
-			$logical_op_list = array('=', '<', '>', '<=', '>=', '<>', '!=', 'LIKE');
-			$where_list = array();
-			foreach ( $where_field_list as $field => $value ) {
-				// See if field has an operator at the end of it, if so, use that
-				// rather than the equals operator, otherwise, use equals by default.
-				$field = trim($field);
-				
-				if ( false !== strpos($field, ' ') ) {
-					// There's a space, see if an operator exists
-					$op_list = explode(' ', $field);
-					if ( count($op_list) != 2 ) {
-						$field = $op_list[0];
-						$operator = $op_list[count($op_list)-1];
-					} else {
-						$field = $op_list[0];
-						$operator = $op_list[1];
-					}
-						
-					if ( false === in_array($operator, $logical_op_list) ) {
-						$operator = '=';
-					}
-				} else {
-					$operator = '=';
-				}
-				
-				$where_list[] = $field . ' ' . $operator . " '" . $this->escape($value) . "'";
-			}
-			$where_sql = " WHERE " . implode(" AND ", $where_list);
+		
+		if ( count($this->_where_field_list) > 0 ) {
+			$where_sql = " WHERE (" . implode(") AND (", $this->_where_field_list) . ")";
 		}
 		
 		return $where_sql;
