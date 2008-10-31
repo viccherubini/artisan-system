fcfd352d5fb5c2fd463cbc0b00ceaadca4e98865
Vic Cherubini <vmc@leftnode.com>
2008-10-31 10:13:41 +0000

    Added the new private method _where() which works for both AND and OR WHERE clauses, re-added orWhere() but made it a @todo, and updated the length of where() to work more succinctly.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@296 c87a4359-4766-4260-b519-6004f3ce7042

--- Sql.php
+++ Sql.php
@@ -2,76 +2,70 @@
 
 Artisan_Library::load('Sql/Exception');
 
-class Artisan_Sql {
+/**
+ * This class allows the creation of a new Sql object to build
+ * queries through chainable commands. It supports SELECT, UPDATE,
+ * INSERT, REPLACE, DELETE, and GENERAL.
+ * @author vmc <vmc@leftnode.com>
+ */
+abstract class Artisan_Sql {
+	///< The SQL that will be built.
 	protected $_sql = NULL;
 	
 	///< The list of fields to use in the WHERE clause.
 	protected $_where_field_list = array();
 	
+	///< The SQL keyword AND
 	const SQL_AND = 'AND';
+	
+	///< The SQL keyword OR
 	const SQL_OR = 'OR';
 	
-
+	/**
+	 * Default constructor for building a new SQL query. This class is abstract and
+	 * can't be built directly.
+	 * @author vmc <vmc@leftnode.com>
+	 * @retval Object New Artisan_Sql object.
+	 */
 	public function __construct() {
-		
 	}
 	
-
+	/**
+	 * Destructor.
+	 * @author vmc <vmc@leftnode.com>
+	 * @retval NULL Destroys the object.
+	 */
 	public function __destruct() {
-
 	}
 	
-	public function where() {		
+	/**
+	 * Creates an AND clause in a WHERE clause.
+	 * @author vmc <vmc@leftnode.com>
+	 * @param $field_op Parameters are not specified, first one is the operation of the field
+	 * @param $value The value to be replaced if there is a ? in the $field_op
+	 * @param $valueN You can specify as many values as there are ?'s in the $field_op.
+	 * @retval Object Returns itself for chainability.
+	 */
+	public function where() {
 		$argv = func_get_args();
-		$argc = count($argv);
-		
-		$where_item = NULL;
-		
-		if ( 1 === $argc ) {
-			$where_item = $argv[0];
-		} elseif ( $argc > 1 ) {
-			$field_op = trim($argv[0]);
-			
-			// Make an array of values to replace
-			$fv_list = array_splice($argv, 1, $argc, array());
-			
-			// Create an array of locations of all ?'s in the string.
-			// Each entry in here will correspond to the index of the array
-			// after str_split() below allowing for easy replacement.
-			$qm_loc = array();
-			$qm_count = 0;
-			$fo_len = strlen($field_op);
-			for ( $i=0; $i<$fo_len; $i++ ) {
-				if ( '?' == $field_op[$i] ) {
-					$qm_loc[] = $i;
-					$qm_count++;
-				}
-			}
-			
-			// Go through and perform all of the replacements
-			$fvl_len = count($fv_list);
-			$field_op = str_split($field_op);
-			if ( $qm_count == $fvl_len ) {
-				for ( $i=0; $i<$fvl_len; $i++ ) {
-					$fv = $this->escape($fv_list[$i]);
-					if ( false === is_numeric($fv) ) {
-						$fv = " '" . $fv . "'";
-					}
-					
-					$field_op[$qm_loc[$i]] = $fv;
-				}
-				
-				$where_item = implode('', $field_op);
-			} else {
-				// There are extra question marks and shouldn't be, unset everything.
-				$where_item = NULL;
-			}
-		}
+		$this->_where(self::SQL_AND, $argv);
 
-		if ( false === empty($where_item) ) {
-			$this->_where_field_list[] = $where_item;
-		}
-		
+		return $this;
+	}
+	
+	/**
+	 * Not yet implemented, but creates an OR clause in a WHERE clause.
+	 * @author vmc <vmc@leftnode.com>
+	 * @todo Finish implementing this and take out stub programming.
+	 * @param $field_op Parameters are not specified, first one is the operation of the field
+	 * @param $value The value to be replaced if there is a ? in the $field_op
+	 * @param $valueN You can specify as many values as there are ?'s in the $field_op.
+	 * @retval Object Returns itself for chainability.
+	 */
+	public function orWhere() {
+		$argv = func_get_args();
+		$argv = array('NOT YET IMPLEMENTED!');
+		$this->_where(self::SQL_AND, $argv);
 		return $this;
 	}
 	
@@ -90,19 +84,79 @@ class Artisan_Sql {
 		return true;
 	}
 	
+	/**
+	 * Builds up the WHERE clause for executing in SELECT and UPDATE queries.
+	 * @author vmc <vmc@leftnode.com>
+	 * @retval string Returns the built WHERE clause.
+	 */
 	public function buildWhereClause() {
 		$where_sql = NULL;
 		
 		if ( count($this->_where_field_list) > 0 ) {
-			$where_sql = " WHERE (" . implode(") AND (", $this->_where_field_list) . ")";
+			$where_sql = " WHERE (" . implode(") " . self::SQL_AND . " (", $this->_where_field_list[self::SQL_AND]) . ")";
+			
+			// Ignore OR for now
+			//$where_sql .= " (" . implode(") " . self::SQL_OR . " (", $this->_where_field_list[self::SQL_OR]) . ")";
 		}
 		
 		return $where_sql;
 	}
 
-	private function _where($type) {
-
-
+	private function _where($type, $field_data) {
+		$argc = count($field_data);
+		
+		if ( count($argc) > 0 ) {
+			$fv_list = array();
+			$field_op = trim($field_data[0]);
+			
+			if ( $argc > 1 ) {
+				// Make an array of values to replace
+				$fv_list = array_splice($field_data, 1, $argc, array());
+			}
+			
+			$where_item = NULL;
+		
+			if ( (true === empty($fv_list) || 0 === count($fv_list)) && false === empty($field_op) ) {
+				$where_item = $field_op;
+			} else {
+				// Create an array of locations of all ?'s in the string.
+				// Each entry in here will correspond to the index of the array
+				// after str_split() below allowing for easy replacement.
+				$qm_loc = array();
+				$qm_count = 0;
+				$fo_len = strlen($field_op);
+				for ( $i=0; $i<$fo_len; $i++ ) {
+					if ( '?' == $field_op[$i] ) {
+						$qm_loc[] = $i;
+						$qm_count++;
+					}
+				}
+		
+				// Go through and perform all of the replacements
+				$fvl_len = count($fv_list);
+				$field_op = str_split($field_op);
+				if ( $qm_count == $fvl_len ) {
+					for ( $i=0; $i<$fvl_len; $i++ ) {
+						$fv = $this->escape($fv_list[$i]);
+						if ( false === is_numeric($fv) ) {
+							$fv = " '" . $fv . "'";
+						}
+				
+						$field_op[$qm_loc[$i]] = $fv;
+					}
+			
+					$where_item = implode('', $field_op);
+				} else {
+					// There are extra question marks and shouldn't be, unset everything.
+					$where_item = NULL;
+				}
+			}
+		
+			// If everything passes, push this item onto the stack.
+			if ( false === empty($where_item) ) {
+				$this->_where_field_list[$type][] = $where_item;
+			}
+		}
 	}
 	
 	
