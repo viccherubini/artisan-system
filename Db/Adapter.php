4016fef26d3fa47706fb0ad40081702f0bef7bd0
Vic Cherubini <vmc@leftnode.com>
2008-11-30 23:30:17 +0000

    Added methods _sanitizeValueList(), insert(), and update().
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@387 c87a4359-4766-4260-b519-6004f3ce7042

--- Db/Adapter.php
+++ Db/Adapter.php
@@ -3,7 +3,7 @@
 
 abstract class Artisan_Db_Adapter {
 	///< The connection to the database.
-	private $CONN = NULL;
+	protected $CONN = NULL;
 	
 	///< The configuration data.
 	private $CONFIG = NULL;
@@ -47,12 +47,96 @@ abstract class Artisan_Db_Adapter {
 		return $this->_is_connected;
 	}
 	
+	public function update($table, $value_list, $where_list = NULL) {
+		$table = trim($table);
+		
+		if ( true === empty($table) ) {
+			throw new Artisan_Db_Exception(ARTISAN_WARNING, 'The table name is empty.', __CLASS__, __FUNCTION__);
+		}
+		
+		if ( false === is_array($value_list) || count($value_list) < 1 ) {
+			throw new Artisan_Db_Exception(ARTISAN_WARNING, 'The value list is not an array or is empty.', __CLASS__, __FUNCTION__);
+		}
+		
+		$value_list = $this->_sanitizeValueList($value_list);
+		
+		$sql = "UPDATE `" . $table . "` SET ";
+		$fl_len = count($value_list)-1;
+		$i=0;
+		foreach ( $value_list as $field => $value ) {
+			$sql .= "`" . $field . "` = " . $value;
+			if ( $i != $fl_len ) {
+				$sql .= ', ';
+			}
+			$i++;
+		}
+		
+		if ( false === empty($where_list) ) {
+			if ( false === is_array($where_list) ) {
+				$where_list = array($where_list);
+			}
+			
+			$sql .= " WHERE (" . implode(") AND (", $where_list) . ")";
+		}
+		
+		try {
+			$this->query($sql);
+		} catch ( Artisan_Db_Exception $e ) {
+			throw $e;
+		}
+	}
+	
+	public function insert($table, $value_list) {
+		$table = trim($table);
+		
+		if ( true === empty($table) ) {
+			throw new Artisan_Db_Exception(ARTISAN_WARNING, 'The table name is empty.', __CLASS__, __FUNCTION__);
+		}
+		
+		if ( false === is_array($value_list) || count($value_list) < 1 ) {
+			throw new Artisan_Db_Exception(ARTISAN_WARNING, 'The value list is not an array or is empty.', __CLASS__, __FUNCTION__);
+		}
+		
+		$value_list = $this->_sanitizeValueList($value_list);
+		
+		$sql = "INSERT INTO `" . $table . "` ";
+		$sql .= " VALUES(" . implode(", ", $value_list) . ")";
+		
+		try {
+			$this->query($sql);
+		} catch ( Artisan_Db_Exception $e ) {
+			throw $e;
+		}
+	}
+	
+	private function _sanitizeValueList($vl) {
+		foreach ( $vl as $i => $value ) {
+			$vl[$i] = $this->escape($value);
+			switch ( strtoupper($value) ) {
+				case NULL: {
+					$vl[$i] = 'NULL';
+					break;
+				}
+				
+				case 'NULL': 
+				case 'NOW()': {
+					$vl[$i] = $value;
+					break;
+				}
+			
+				default: {
+					$vl[$i] = "'" . $value . "'";
+					break;
+				}
+			}
+		}
+		
+		return $vl;
+	}
+	
 	abstract public function connect();
 	abstract public function disconnect();
 	abstract public function close();
 	abstract public function query($sql);
-	//abstract public function update($table, $value_list, $where_sql = NULL);
-	//abstract public function insert($table, $value_list);
 	abstract public function escape($value);
-	
 }
