f2af91753556dceb0873f725e25dd339b0d45eb4
Vic Cherubini <vmc@leftnode.com>
2008-11-30 05:32:58 +0000

    Added some temporary text, going to move this file shortly.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@350 c87a4359-4766-4260-b519-6004f3ce7042

--- Db/Mysqli.php
+++ Db/Mysqli.php
@@ -2,10 +2,10 @@
 
 require_once 'Db/Adapter.php';
 
-class Artisan_Db_Mysqli extends Artisan_Db_Adapter {
+class Artisan_Db_Adapter_Mysqli extends Artisan_Db_Adapter {
 
 	public function __construct(Artisan_Config &$CFG) {
-	
+		echo 'In ' . __CLASS__ . '::' . __FUNCTION__ . '<br>';
 	}
 	
 	public function __destruct() {
