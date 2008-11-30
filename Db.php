fdc05c42b1854f79c1b30745f8f809bc9972d411
Vic Cherubini <vmc@leftnode.com>
2008-11-30 07:48:01 +0000

    Added Doxygen comment for factory() and updated the way Artisan_Library::load() calls the appropriate method
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@364 c87a4359-4766-4260-b519-6004f3ce7042

--- Db.php
+++ Db.php
@@ -1,8 +1,14 @@
 <?php
 
-require_once 'Artisan/Db/Exception.php';
+/**
+ * @see Artisan_Library
+ */
+require_once 'Artisan/Library.php';
 
-require_once 'Artisan/Functions/Array.php';
+/**
+ * @see Artisan_Db_Exception
+ */
+require_once 'Artisan/Db/Exception.php';
 
 /**
  * The abstract Database class from which other database classes are extended.
@@ -14,15 +20,42 @@ class Artisan_Db {
 	///< Holds the database configuration information, must be of type Artisan_Config.
 	protected $CONFIG = NULL;
 
+	///< The default adapter.
 	const DEFAULT_ADAPTER = 'Artisan_Db_Adapter';
 
+	///< The name for the Mysqli adapter.
 	const DB_MYSQLI = 'mysqli';
+	
+	///< The name for the Postgres adapter.
 	const DB_POSTGRES = 'postgres';
+	
+	///< The name for the Postgresql adapter (alias for the Postgres adapter).
 	const DB_POSTGRESQL = 'postgresql';
+	
+	///< The name for the PDO (PHP Data Objects) adapter.
 	const DB_PDO = 'pdo';
+	
+	///< The name for the Sqlite adapter.
 	const DB_SQLITE = 'sqlite';
 
-
+	/**
+	 * Builds a database object based on the adapter specified
+	 * in the configuration.
+	 * @author vmc <vmc@leftnode.com>
+	 * @param $CFG Configuration object of type Artisan_Config passed by reference. Must
+	 * have following values:
+	 * @code
+	 * // Required:
+	 * $CFG->server
+	 * $CFG->username
+	 * $CFG->password
+	 * $CFG->database
+	 * // Optional
+	 * $CFG->port
+	 * @endcode
+	 * @throw Artisan_Db_Exception If the adapter is not present in the configuration.
+	 * @retval mixed New database object if successfully created, NULL otherwise.
+	 */
 	static public function factory(Artisan_Config &$CFG) {
 		// Ensure an adapter exists
 		if ( false === asfw_exists('adapter', $CFG) ) {
@@ -32,16 +65,16 @@ class Artisan_Db {
 		// Load in the appropriate file
 		$adapter = strtolower(trim($CFG->adapter));
 		$adapter = str_replace(' ', '_', ucwords(str_replace('_', ' ', $adapter)));
-		
-		$file = $adapter;
 		$adapter = self::DEFAULT_ADAPTER . '_' . $adapter;
 		
-		/*
-		 try {
-			Artisan_Library::load($file, 'Db');
-		} catch ( Artisan_Exception $e ) {
-			throw $e;
+		Artisan_Library::load($adapter);
+		
+		if ( true === class_exists($adapter) ) {
+			$db = new $adapter($CFG);
+			
+			return $db;
 		}
-		*/
+		
+		return NULL;
 	}
 }
