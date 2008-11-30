d271bbda9d68ca38bcb09712e6650766d8570a98
Vic Cherubini <vmc@leftnode.com>
2008-11-30 21:19:55 +0000

    Wrote most of the abstract methods: disconnect(), close(), connect(), query(), update(), insert(), and escape().
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@371 c87a4359-4766-4260-b519-6004f3ce7042

--- Db/Adapter/Mysqli.php
+++ Db/Adapter/Mysqli.php
@@ -11,13 +11,19 @@ require_once 'Artisan/Db/Adapter.php';
 require_once 'Artisan/Db/Exception.php';
 
 /**
+ * @see Artisan_Db_Result_Mysqli
+ */
+require_once 'Artisan/Db/Result/Mysqli.php';
+
+/**
  * This adapter class handles a connection to a Mysql database.
  * @author vmc <vmc@leftnode.com>
  */
 class Artisan_Db_Adapter_Mysqli extends Artisan_Db_Adapter {
 
 	public function __construct(Artisan_Config &$CFG) {
-		echo 'In ' . __CLASS__ . '::' . __FUNCTION__ . '<br>';
+		$this->CONFIG = $CFG;
+		$this->CONN = NULL;
 	}
 	
 	public function __destruct() {
@@ -33,4 +39,75 @@ class Artisan_Db_Adapter_Mysqli extends Artisan_Db_Adapter {
 		return __CLASS__;
 	}
 
+
+
+
+	public function connect() {
+		$server = $this->CONFIG->server;
+		$username = $this->CONFIG->username;
+		$password = $this->CONFIG->password;
+		$database = $this->CONFIG->database;
+		
+		$port = 3306;
+		if ( true === asfw_exists('port', $this->CONFIG) ) {
+			if ( intval($this->CONFIG->port) > 0 ) {
+				$port = intval($this->CONFIG->port);
+			}
+		}
+		
+		// Although generally against supressing errors, the @ is
+		// to supress a misconnection error
+		// to allow the framework to handle it gracefully
+		$this->CONN = @new mysqli($server, $username, $password, $database, $port);
+
+		if ( 0 != mysqli_connect_errno() || false === $this->CONN ) {
+			$this->_is_connected = false;
+			throw new Artisan_Db_Exception(ARTISAN_WARNING, mysqli_connect_error(), __CLASS__, __FUNCTION__);
+		}
+
+		$this->_is_connected = true;
+		return $this->CONN;
+	}
+
+	public function disconnect() {
+		if ( true === $this->_is_connected ) {
+			$this->CONN->close();
+			$this->CONN = NULL;
+			$this->_is_connected = false;
+		}
+
+		return true;
+	}
+	
+	public function close() {
+		$this->disconnect();
+	}
+	
+	
+	public function query($sql) {
+		$sql = trim($sql);
+		
+		if ( true === empty($sql) ) {
+			throw new Artisan_Db_Exception(ARTISAN_WARNING, 'The SQL query is empty.', __CLASS__, __FUNCTION__);
+		}
+		
+		$result = $this->CONN->query($sql);
+		if ( true === is_object($result) && true === $result instanceof mysqli_result ) {
+			return new Artisan_Db_Result_Mysqli($result);
+		}
+		
+		return $result;
+	}
+	
+	public function update($table, $value_list, $where_sql = NULL) {
+	
+	}
+	
+	public function insert($table, $value_list) {
+	
+	}
+	
+	public function escape($value) {
+		return $this->CONN->real_escape_string($value);
+	}
 }
