a84d44f912b5216aee7c1f633a42fca68ae56dd1
Vic Cherubini <vmc@leftnode.com>
2008-12-01 11:57:27 +0000

    Added the methods for query() and affectedRows()
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@424 c87a4359-4766-4260-b519-6004f3ce7042

--- Db/Sql/Insert/Mysqli.php
+++ Db/Sql/Insert/Mysqli.php
@@ -8,11 +8,24 @@ class Artisan_Db_Sql_Insert_Mysqli extends Artisan_Db_Sql_Insert {
 	}
 	
 	public function query() {
-	
+		$this->build();
+		
+		$result = false;
+		if ( true === $this->CONN instanceof mysqli ) {
+			$result = $this->CONN->query($this->_sql);
+		}
+		
+		if ( false === $result ) {
+			throw new Artisan_Db_Sql_Exception(ARTISAN_WARNING, 'Failed to execute query: "' . $sql . '"', __CLASS__, __FUNCTION__);
+		}
+		
+		return $this;
 	}
 	
 	public function affectedRows() {
-	
+		if ( true === $this->CONN instanceof mysqli ) {
+			return $this->CONN->affected_rows;
+		}
 	}
 	
 	public function escape($value) {
