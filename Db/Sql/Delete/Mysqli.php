712b073f9637bbb6c7eab7546f375e98ae42ba24
Vic Cherubini <vmc@leftnode.com>
2008-12-01 04:00:00 +0000

    Mysqli class for creating a DELETE query.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@413 c87a4359-4766-4260-b519-6004f3ce7042

--- Db/Sql/Delete/Mysqli.php
+++ Db/Sql/Delete/Mysqli.php
@@ -3,5 +3,22 @@
 require_once 'Artisan/Db/Sql/Delete.php';
 
 class Artisan_Db_Sql_Delete_Mysqli extends Artisan_Db_Sql_Delete {
-
+	public function __construct(mysqli &$CONN) {
+		$this->CONN = &$CONN;
+	}
+	
+	public function query() {
+	
+	}
+	
+	public function affectedRows() {
+	
+	}
+	
+	public function escape($value) {
+		if ( true === $this->CONN instanceof mysqli ) {
+			return $this->CONN->real_escape_string($value);
+		}
+		return addslashes($value);
+	}
 }
