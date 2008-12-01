77a867b69dd80abcca3f1322e00676e777fbf537
Vic Cherubini <vmc@leftnode.com>
2008-12-01 01:58:24 +0000

    Removed the transactional methods to redo, added the new and improved query() method.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@399 c87a4359-4766-4260-b519-6004f3ce7042

--- Db/Mysqli.php
+++ Db/Mysqli.php
@@ -1,14 +1,10 @@
 <?php
 
-// Load in its own Parameterized Sql classes
-//Artisan_Library::load('Sql/Mysqli/Select');
-//Artisan_Library::load('Sql/Mysqli/Update');
-//Artisan_Library::load('Sql/Mysqli/Insert');
-//Artisan_Library::load('Sql/Mysqli/Delete');
-//Artisan_Library::load('Sql/Mysqli/General');
-//Artisan_Library::load('Sql/Mysqli/Replace');
+require_once 'Artisan/Db.php';
 
-require_once 'Artisan/Database.php';
+require_once 'Artisan/Db/Exception.php';
+
+require_once 'Artisan/Db/Result/Mysqli.php';
 
 /**
  * The Mysqli class for connecting to a mysql database.
@@ -18,27 +14,6 @@ class Artisan_Db_Mysqli extends Artisan_Db {
 	///< The connection to the database (Mysqli Object)
 	private $CONN = NULL;
 
-	///< An instance of a mysqli_result class to build parameterized query.
-	//private $STATEMENT = NULL;
-
-	///< The instance of the Artisan_Sql_Select_Mysqli class for executing queries.
-	public $select = NULL;
-	
-	///< The instance of the Artisan_Sql_Insert_Mysqli class for executing queries.
-	public $insert = NULL;
-	
-	///< The instance of the Artisan_Sql_Update_Mysqli class for executing queries.
-	public $update = NULL;
-
-	///< The instance of the Artisan_Sql_Delete_Mysqli class for executing queries.
-	public $delete = NULL;
-
-	///< The instance of the Artisan_Sql_General_Mysqli class for executing queries.
-	public $general = NULL;
-	
-	///< The instance of the Artisan_Sql_Replace_Mysqli class for executing queries.
-	public $replace = NULL;
-	
 	/**
 	 * Destructor, disconnects from the database if currently connected.
 	 * @author vmc <vmc@leftnode.com>
@@ -78,19 +53,11 @@ class Artisan_Db_Mysqli extends Artisan_Db {
 
 		if ( 0 != mysqli_connect_errno() || false === $this->CONN ) {
 			$this->_is_connected = false;
-			throw new Artisan_Database_Exception(ARTISAN_WARNING, mysqli_connect_error(), __CLASS__, __FUNCTION__);
+			throw new Artisan_Db_Exception(ARTISAN_WARNING, mysqli_connect_error(), __CLASS__, __FUNCTION__);
 		}
 
 		$this->_is_connected = true;
-		
-		// Set the connection for the parameterized SQL
-		$this->select = new Artisan_Sql_Select_Mysqli($this->CONN);
-		$this->update = new Artisan_Sql_Update_Mysqli($this->CONN);
-		$this->insert = new Artisan_Sql_Insert_Mysqli($this->CONN);
-		$this->delete = new Artisan_Sql_Delete_Mysqli($this->CONN);
-		$this->replace = new Artisan_Sql_Replace_Mysqli($this->CONN);
-		$this->general = new Artisan_Sql_General_Mysqli($this->CONN);
-		
+
 		return $this->CONN;
 	}
 
@@ -109,115 +76,25 @@ class Artisan_Db_Mysqli extends Artisan_Db {
 		return true;
 	}
 
-	/**
-	 * Whether or not a connection to the database exists.
-	 * @author vmc <vmc@leftnode.com>
-	 * @retval boolean True if connected, false otherwise.
-	 */
-	public function isConnected() {
-		return $this->_is_connected;
-	}
-
-	/**
-	 * Prepare a query for execution and then execute it.
-	 * @author vmc <vmc@leftnode.com>
-	 * @param $sql The SQL to prepare to execute.
-	 * @throw Artisan_Database_Exception If the statement is currently fetching all of its data, it can not be restarted.
-	 * @throw Artisan_Database_Exception If the query is empty.
-	 */
-	public function prepareExecute($sql) {
-		if ( true === $this->STATEMENT instanceof mysqli_result ) {
-			throw new Artisan_Database_Exception(ARTISAN_WARNING, 
-				'A statement has not finished fetching all of its data. Please close the current statement.', __CLASS__, __FUNCTION__);
-		}
-
+	public function query($sql) {
 		$sql = trim($sql);
+		
 		if ( true === empty($sql) ) {
-			throw new Artisan_Database_Exception(ARTISAN_WARNING, 'The SQL statement is empty.', __CLASS__, __FUNCTION__);
-		}
-	
-		$this->STATEMENT = $this->CONN->prepare($sql);
-		
-		return $this;
-	}
-	
-
-	/**
-	 * Start transtional queries.
-	 * @author vmc <vmc@leftnode.com>
-	 * @todo Finish implementing this.
-	 * @retval boolean Returns true.
-	 */
-	protected function _start() {
-		// Turn autocommit off
-		$this->CONN->autocommit(false);
-		$this->_transaction_started = true;
-		
-		return true;
-	}
-
-	/**
-	 * Rolls back any failed transitional queries.
-	 * @author vmc <vmc@leftnode.com>
-	 * @todo Finish implementing this, test the rollback and throw an exception.
-	 * @retval boolean Returns true.
-	 */
-	protected function _rollback() {
-		if ( true === $this->_transaction_started ) {
-			$this->CONN->rollback();
+			throw new Artisan_Db_Exception(ARTISAN_WARNING, 'The SQL statement is empty.', __CLASS__, __FUNCTION__);
 		}
 		
-		return true;
-	}
-
-	/**
-	 * Commit a series of SQL transactions.
-	 * @author vmc <vmc@leftnode.com>
-	 * @todo Finish implementing this, test the commit and throw an exception.
-	 * @retval boolean Returns true;
-	 */
-	protected function _commit() {
-		if ( true === $this->_transaction_started ) {
-			$this->CONN->commit();
-		}
-		
-		return true;
-	}
-
-	/**
-	 * Support for transactional queries. If any of the queries fail, all of them fail,
-	 * and the database is returned to its original state.
-	 * @author vmc <vmc@leftnode.com>
-	 * @param $query_list A list of queries to queue and execute. Can be an array of Artisan_Sql objects or an array of string queries.
-	 * @retval boolean Returns true if successfully execute, false otherwise.
-	 */
-	public function queue($query_list) {
-		$this->_start();
-
-		if ( false === is_array($query_list) || count($query_list) < 1 ) {
-			$this->_transaction_started = false;
-			return false;
-		}
-
-		$error = false;
-		$len = count($query_list);
-		for ( $i=0; $i<$len; $i++ ) {
-			if ( true === $query_list[$i] instanceof Artisan_Sql ) {
-				//$success = $this->query($query_list[$i]);
-
-				if ( false === $success ) {
-					$error = true;
-					break;
-				}
+		if ( true === is_object($this->CONN) ) {
+			$result = $this->CONN->query($sql);
+			
+			if ( true === $result instanceof mysqli_result ) {
+				return new Artisan_Db_Result_Mysqli($result);
 			}
 		}
-
-		if ( true === $error ) {
-			$this->_rollback();
-		} else {
-			$this->_commit();
+		
+		if ( false === $result ) {
+			throw new Artisan_Db_Exception(ARTISAN_WARNING, 'Failed to execute query: "' . $sql . '"', __CLASS__, __FUNCTION__);
 		}
-
-		return $error;
+		
+		return $result;
 	}
 }
