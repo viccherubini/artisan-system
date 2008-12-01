b7e6fc5482b6863bed2d0a2bc1843af26c9dcc4a
Vic Cherubini <vmc@leftnode.com>
2008-12-01 03:22:11 +0000

    All new classes for the ways to execute different queries against the database.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@404 c87a4359-4766-4260-b519-6004f3ce7042

---
+++ Db/Sql/Delete/Mysqli.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once 'Artisan/Db/Sql/Delete.php';
+
+class Artisan_Db_Sql_Delete_Mysqli extends Artisan_Db_Sql_Delete {
+
+}
---
+++ Db/Sql/Update/Mysqli.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once 'Artisan/Db/Sql/Update.php';
+
+class Artisan_Db_Sql_Update_Mysqli extends Artisan_Db_Sql_Update {
+
+}
