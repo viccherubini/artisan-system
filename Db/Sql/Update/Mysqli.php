14a46d2a56139d16f824e81f9aec7df7edc14892
Vic Cherubini <vmc@leftnode.com>
2008-12-01 03:59:44 +0000

    Mysqli class for creating an UPDATE query.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@412 c87a4359-4766-4260-b519-6004f3ce7042

--- Db/Sql/Update/Mysqli.php
+++ Db/Sql/Update/Mysqli.php
@@ -3,5 +3,22 @@
 require_once 'Artisan/Db/Sql/Update.php';
 
 class Artisan_Db_Sql_Update_Mysqli extends Artisan_Db_Sql_Update {
-
+	public function __construct(mysqli &$CONN) {
+		$this->CONN = &$CONN;
+	}
+	
+	public function query() {
+	
+	}
+	
+	public function affectedRows() {
+	
+	}
+	
+	public function escape($value) {
+		if ( true === $this->CONN instanceof mysqli ) {
+			return $this->CONN->real_escape_string($value);
+		}
+		return addslashes($value);
+	}
 }
