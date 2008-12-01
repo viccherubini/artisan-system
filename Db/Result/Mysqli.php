fed68a4883752a71a7ac107c7fd20f39cb4e86f1
Vic Cherubini <vmc@leftnode.com>
2008-12-01 04:00:28 +0000

    Added the code for the method fetch() that returns a single row or field at a time.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@414 c87a4359-4766-4260-b519-6004f3ce7042

--- Db/Result/Mysqli.php
+++ Db/Result/Mysqli.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once 'Artisan/Functions/Array.php';
+
 require_once 'Artisan/Db/Result.php';
 
 class Artisan_Db_Result_Mysqli extends Artisan_Db_Result {
@@ -10,7 +12,27 @@ class Artisan_Db_Result_Mysqli extends Artisan_Db_Result {
 	}
 
 	public function fetch($field = NULL) {
+		if ( true === $this->RESULT instanceof mysqli_result ) {
+			$data = $this->RESULT->fetch_assoc();
+			if ( true === is_null($data) ) {
+				$this->free();
+			} else {
+				reset($data);
+				
+				// Check if only one field was returned, if so, return that
+				if ( 1 === count($data) ) {
+					$data = current($data);
+				} else {
+					if ( false === empty($field) && true === asfw_exists($field, $data) ) {
+						$data = $data[$field];				
+					}
+				}			
+			}
 
+			return $data;
+		}
+		
+		return array();
 	}
 
 	public function fetchAll($key_on_primary = false) {
