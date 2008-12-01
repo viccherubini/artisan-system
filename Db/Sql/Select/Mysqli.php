dee93cbdd05437cde8de4727a5e88e7eb66bfd01
Vic Cherubini <vmc@leftnode.com>
2008-12-01 03:59:32 +0000

    Mysqli class for creating a SELECT query.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@411 c87a4359-4766-4260-b519-6004f3ce7042

--- Db/Sql/Select/Mysqli.php
+++ Db/Sql/Select/Mysqli.php
@@ -1,7 +1,39 @@
 <?php
 
+require_once 'Artisan/Db/Result/Mysqli.php';
+
 require_once 'Artisan/Db/Sql/Select.php';
 
-class Artisan_Db_Sql_Select_Mysqli extends Artisan_Db_Sql_Select {
+require_once 'Artisan/Db/Exception.php';
 
+class Artisan_Db_Sql_Select_Mysqli extends Artisan_Db_Sql_Select {
+	
+	public function __construct(mysqli &$CONN) {
+		$this->CONN = &$CONN;
+	}
+	
+	public function query() {
+		$this->build();
+		
+		if ( true === $this->CONN instanceof mysqli ) {
+			$result = $this->CONN->query($this->_sql);
+			
+			if ( true === $result instanceof mysqli_result ) {
+				return new Artisan_Db_Result_Mysqli($result);
+			}
+		}
+		
+		if ( false === $result ) {
+			throw new Artisan_Db_Exception(ARTISAN_WARNING, 'Failed to execute query: "' . $sql . '"', __CLASS__, __FUNCTION__);
+		}
+		
+		return $result;
+	}
+	
+	public function escape($value) {
+		if ( true === $this->CONN instanceof mysqli ) {
+			return $this->CONN->real_escape_string($value);
+		}
+		return addslashes($value);
+	}
 }
