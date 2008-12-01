5da940f2b3c8d6546fb2b7c15420b4926abbc549
Vic Cherubini <vmc@leftnode.com>
2008-12-01 04:02:11 +0000

    Copied over Sql/Delete.php to this class.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@419 c87a4359-4766-4260-b519-6004f3ce7042

--- Db/Sql/Delete.php
+++ Db/Sql/Delete.php
@@ -2,6 +2,95 @@
 
 require_once 'Artisan/Db/Sql.php';
 
+/**
+ * The abstract Insert class for building a query to delete data from a database.
+ * @author vmc <vmc@leftnode.com>
+ */
 abstract class Artisan_Db_Sql_Delete extends Artisan_Db_Sql {
+	///< The main table the query is selecting FROM.
+	protected $_from_table = NULL;
+	
+	///< The number of rows that should be deleted
+	protected $_limit = 0;
+	
+	/**
+	 * Default constructor for building a new INSERT query.
+	 * @author vmc <vmc@leftnode.com>
+	 * @retval Object New Artisan_Sql_Delete object.
+	 */
+	public function __construct() {
+		$this->_sql = NULL;
+	}
+	
+	/**
+	 * Destructor.
+	 * @author vmc <vmc@leftnode.com>
+	 * @retval NULL Destroys the object.
+	 */
+	public function __destruct() {
+		unset($this->_sql);
+	}
+	
+	/**
+	 * Specifies what table to delete from.
+	 * @author vmc <vmc@leftnode.com>
+	 * @param $table The name of the table
+	 * @retval Object Returns itself for chainability.
+	 */
+	public function from($table) {
+		$table = trim($table);
+		$this->_from_table = $table;
+		return $this;
+	}
+	
+	/**
+	 * Sets the limit for the number of rows that should be deleted.
+	 * @author vmc <vmc@leftnode.com>
+	 * @param $limit Positive integer limit.
+	 * @retval Object Returns itself for chainability.
+	 */
+	public function limit($limit) {
+		$limit = abs(intval($limit));
+		$this->_limit = $limit;
+		
+		return $this;
+	}
+	
+	/**
+	 * Builds the query to execute.
+	 * @author vmc <vmc@leftnode.com>
+	 * @retval boolean Returns true.
+	 */
+	public function build() {
+		$where_sql = $this->buildWhereClause($this->_where_field_list);
+		
+		$limit_sql = NULL;
+		if ( $this->_limit > 0 ) {
+			$limit_sql = " LIMIT " . $this->_limit;
+		}
+		
+		$delete_start = "DELETE FROM `" . $this->_from_table . "` ";
+		$this->_sql = $delete_start . $where_sql . $limit_sql;
+	}
 
+	/**
+	 * Executes the query against the database.
+	 * @author vmc <vmc@leftnode.com>
+	 * @retval Object Returns an instance of itself for chaining.
+	 */
+	abstract public function query();
+	
+	/**
+	 * Returns the number of rows inserted.
+	 * @author vmc <vmc@leftnode.com>
+	 * @retval int Returns the number of rows affected by the DELETE.
+	 */
+	abstract public function affectedRows();
+
+	/**
+	 * Escapes a string based on the character set of the current connection.
+	 * @author vmc <vmc@leftnode.com>
+	 * @retval string Returns a context escaped string.
+	 */
+	abstract public function escape($value);
 }
