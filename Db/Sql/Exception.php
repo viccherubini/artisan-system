271aa6967a6ec875ab906325c793a3582feed920
Vic Cherubini <vmc@leftnode.com>
2008-12-01 03:59:10 +0000

    The new class Artisan_Db_Sql_Exception for throwing exceptions in the SQL layer.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@410 c87a4359-4766-4260-b519-6004f3ce7042

---
+++ Db/Sql/Exception.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once 'Artisan/Exception.php';
+
+/**
+ * Database SQL Exception Class.
+ * @author vmc <vmc@leftnode.com>
+ */
+class Artisan_Db_Sql_Exception extends Artisan_Exception {
+}
