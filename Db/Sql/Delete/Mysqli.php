c51df838de81b2c7217da431ce59b41278b6b232
Vic Cherubini <vmc@leftnode.com>
2008-12-02 11:27:19 +0000

    Removed the content of these classes as all of the base methods can be defined elsewhere, thus, build() can be overwritten here if necessary.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@427 c87a4359-4766-4260-b519-6004f3ce7042

--- Db/Sql/Delete/Mysqli.php
+++ Db/Sql/Delete/Mysqli.php
@@ -3,35 +3,4 @@
 require_once 'Artisan/Db/Sql/Delete.php';
 
 class Artisan_Db_Sql_Delete_Mysqli extends Artisan_Db_Sql_Delete {
-	public function __construct(mysqli &$CONN) {
-		$this->CONN = &$CONN;
-	}
-	
-	public function query() {
-		$this->build();
-		
-		$result = false;
-		if ( true === $this->CONN instanceof mysqli ) {
-			$result = $this->CONN->query($this->_sql);
-		}
-		
-		if ( false === $result ) {
-			throw new Artisan_Db_Sql_Exception(ARTISAN_WARNING, 'Failed to execute query: "' . $sql . '"', __CLASS__, __FUNCTION__);
-		}
-		
-		return $this;
-	}
-	
-	public function affectedRows() {
-		if ( true === $this->CONN instanceof mysqli ) {
-			return $this->CONN->affected_rows;
-		}
-	}
-	
-	public function escape($value) {
-		if ( true === $this->CONN instanceof mysqli ) {
-			return $this->CONN->real_escape_string($value);
-		}
-		return addslashes($value);
-	}
 }
