e6fa7d07cf9eae7db51a0d1c41db8cfcc7395484
Vic Cherubini <vmc@leftnode.com>
2008-12-05 03:27:34 +0000

    Required the correct files for #58 and renamed all of the Database values to Db.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@444 c87a4359-4766-4260-b519-6004f3ce7042

--- Cache/Database.php
+++ Cache/Database.php
@@ -1,11 +1,16 @@
 <?php
 
 /**
+ * @see Artisan_Cache
+ */
+require_once 'Artisan/Cache.php';
+
+/**
  * Loads cached data from the database.
  * @author vmc <vmc@leftnode.com>
  * @todo Implement this!
  */
-class Artisan_Cache_Database extends Artisan_Cache {
+class Artisan_Cache_Db extends Artisan_Cache {
 	///< Database instance passed into the class. Assumes the database already has a connection.
 	private $DB = NULL;
 	
@@ -15,7 +20,7 @@ class Artisan_Cache_Database extends Artisan_Cache {
 	 * @param $DB The database object to store data into, assumes it is already connected.
 	 * @retval object The new Artisan_Cache_Database object.
 	 */
-	public function __construct(Artisan_Database &$DB) {
+	public function __construct(Artisan_Db &$DB) {
 		// We can only assume the database has a current connection
 		// as we don't want to attempt to connect.
 		$this->DB = &$DB;
