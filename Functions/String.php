eb75c59fa0756063c4eb159db7cf7718a511aeb7
Vic Cherubini <vmc@leftnode.com>
2008-12-08 02:45:54 +0000

    Added methods asfw_rename_controller_method(), asfw_controller_create_base_uri(), and asfw_strip_end_slashes() for managing the controller values.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@472 c87a4359-4766-4260-b519-6004f3ce7042

--- Functions/String.php
+++ Functions/String.php
@@ -18,7 +18,7 @@ function asfw_first_letter($word) {
  * Renames a controller string to be safe for PHP execution.
  * @author vmc <vmc@leftnode.com>
  * @param $controller The name of the controller to rename.
- * @retval string The new safe controller name.,
+ * @retval string The new safe controller name.
 */
 function asfw_rename_controller($controller) {
 	$controller = strtolower(trim($controller));
@@ -31,6 +31,58 @@ function asfw_rename_controller($controller) {
 	
 	return $controller;
 }
+
+/**
+ * Renames a method in a controller, all lowercase with underscores for method names.
+ * @author vmc <vmc@leftnode.com>
+ * @param $method The name of the method to rename.
+ * @retval string The new method name.
+ */
+function asfw_rename_controller_method($method) {
+	$method = preg_replace('/[^a-z_0-9]/i', NULL, $method);
+	return strtolower($method);
+}
+
+/**
+ * Removes any of the query string data from the request_uri to build
+ * the controller/method/arguments heirarchy.
+ * @author vmc <vmc@leftnode.com>
+ * @param $uri The URI to split by a ?.
+ * @retval string Returns the first part of the URI.
+ */
+function asfw_controller_create_base_uri($uri) {
+	$uri = explode('?', $uri);
+	return current($uri);
+}
+
+/**
+ * Removes the first / and last / off of a string. For example:
+ * /index.php/controller/method/value1/ becomes
+ * index.php/controller/method/value1
+ * @author vmc <vmc@leftnode.com>
+ * @param $string The string to sanitize.
+ * @retval string Returns the sanitized string.
+ */
+function asfw_strip_end_slashes($string) {
+	$last = strlen($string)-1;
+	if ( $last <= 0 ) {
+		return NULL;
+	}
+
+	// Strip off the last / if it exists
+	if ( $string[$last] == '/' ) {
+		$string = substr($string, 0, $last);
+	}
+
+	// Strip off the first / if it exists
+	if ( $string[0] == '/' ) {
+		$string = substr($string, 1);
+	}
+	
+	return $string;
+}
+
+
 
 function asfw_class_name_to_file_name($class_name) {
 	$class_name = strtolower(trim($class_name));
