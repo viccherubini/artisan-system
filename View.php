823015b1c8edefa991054be2dd707dcd0c18a726
Vic Cherubini <vmc@leftnode.com>
2008-12-13 13:03:16 +0000

    Added some default methods for playing around with.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@487 c87a4359-4766-4260-b519-6004f3ce7042

--- View.php
+++ View.php
@@ -2,5 +2,31 @@
 
 
 class Artisan_View {
+	private $_theme_dir = NULL;
+	private $_theme_name = NULL;
+	private $_view_dir = NULL;
 
+	public function __construct() {
+
+	}
+
+	public function setViewDir($view_dir) {
+		$this->_view_dir = trim($view_dir);
+	}
+
+	public function setThemeDir($theme_dir) {
+		$this->_theme_dir = trim($theme_dir);
+	}
+
+	public function setTheme($theme_name) {
+		$this->_theme_name = $theme_name;
+	}
+
+	public function getThemeCss($css_name) {
+		return ( $this->_theme_dir . DIRECTORY_SEPARATOR . $this->_theme_name . DIRECTORY_CSS . $css_name . '.css' );
+	}
+
+	public function view($view_name) {
+		
+	}
 }
