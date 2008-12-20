afbddc7e1b2fb78905cdee2616097f77d7702f9e
Vic Cherubini <vmc@leftnode.com>
2008-12-20 05:21:28 +0000

    Stop adding newlines, geany, otherwise you're awesome. And added row() to the abstract result class
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@511 c87a4359-4766-4260-b519-6004f3ce7042

--- Artisan/Db/Result.php
+++ Artisan/Db/Result.php
@@ -1,6 +1,7 @@
 <?php
 
 abstract class Artisan_Db_Result {
+	abstract public function row($offset);
 	abstract public function fetch($field = NULL);
 	abstract public function fetchAll($key_on_primary = false);
 	abstract public function free();
