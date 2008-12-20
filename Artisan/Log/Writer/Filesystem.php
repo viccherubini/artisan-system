eff2cc7df5d7a98ef7622032d57403e53de16291
Vic Cherubini <vmc@leftnode.com>
2008-12-20 05:52:18 +0000

    Added some Doxygen comments for #1
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@515 c87a4359-4766-4260-b519-6004f3ce7042

--- Artisan/Log/Writer/Filesystem.php
+++ Artisan/Log/Writer/Filesystem.php
@@ -5,9 +5,19 @@
  * @author vmc <vmc@leftnode.com>
  */
 class Artisan_Log_Filesystem extends Artisan_Log_Writer {
+	///< The name of the directory to save the file in.
 	private $_save_dir = NULL;
+	
+	///< The name of the log file.
 	private $_log_name = NULL;
 	
+	/**
+	 * Default constructor to build a filesystem writer.
+	 * @author vmc <vmc@leftnode.com>
+	 * @param $location The location directory to write the log in, must be a valid directory.
+	 * @param $log_name The name of the log file to write.
+	 * @retval Object Returns new Artisan_Log_Filesystem.
+	 */
 	public function __construct($location, $log_name) {
 		$location = trim($location);
 		if ( true === is_dir($location) ) {
