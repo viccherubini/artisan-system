ba931cf864efe8f7c9b2583797997e429d868233
Vic Cherubini <vmc@leftnode.com>
2008-12-20 05:46:22 +0000

    Added base class and functions for #1
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@513 c87a4359-4766-4260-b519-6004f3ce7042

---
+++ Artisan/Log/Writer/Filesystem.php
@@ -0,0 +1,28 @@
+<?php
+
+
+class Artisan_Log_Filesystem extends Artisan_Log_Writer {
+	private $_save_dir = NULL;
+	private $_log_name = NULL;
+	
+	public function __construct($location, $log_name) {
+		$location = trim($location);
+		if ( true === is_dir($location) ) {
+			$this->_save_dir = $location;
+		}
+		
+		$this->_log_name = trim($log_name);
+	}
+	
+	public function flush(&$log) {
+		if ( 0 === count($log) ) {
+			return true;
+		}
+
+		foreach ( $log as $l ) {
+			// Write log data to the file
+		}
+		
+		return true;
+	}
+}
