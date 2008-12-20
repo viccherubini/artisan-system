bd81745ac5f0a29a36608923f524e52b76227f12
Vic Cherubini <vmc@leftnode.com>
2008-12-20 22:05:24 +0000

    Removed the old _init() method since that is in Artisan_Vo and renamed VO to Vo to reflect the Artisan_Vo name change.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@528 c87a4359-4766-4260-b519-6004f3ce7042

--- Artisan/Config.php
+++ Artisan/Config.php
@@ -6,52 +6,20 @@
 require_once 'Artisan/Config/Exception.php';
 
 /**
- * @see Artisan_VO
+ * @see Artisan_Vo
  */
-require_once 'Artisan/VO.php';
+require_once 'Artisan/Vo.php';
 
 /**
  * This class holds all configuration data that can come from different sources.
  * @author vmc <vmc@leftnode.com>
  */
-abstract class Artisan_Config extends Artisan_VO {
+abstract class Artisan_Config extends Artisan_Vo {
 	/**
 	 * Load the configuration.
 	 */
 	abstract protected function _load($source);
 
-	/**
-	 * Take an array and turn it into into an object like:
-	 * @author vmc <vmc@leftnode.com>
-	 * @code
-	 * $arr = array(
-	 *     'a' => 'value',
-	 *     'b' => 'value2',
-	 *     'c' => array( 'd' => 'value3' )
-	 * );
-	 * @endcode
-	 * To:
-	 * $object->a has the value 'value', and
-	 * $object->c->d has the value 'value3' in it.
-	 * This way, one can easily internalize a config
-	 * array to access each element easily.
-	 * @retval NULL Returns nothing.
-	 */
-	//protected function _init($root) {
-		/*
-		foreach ( $root as $k => $v ) {
-			if ( true === is_array($v) ) {
-				$this->$k = $t;
-				$this->_init($v);
-			} else {
-				$this->$k = $v;
-			}
-		}
-		*/
-		
-		//$this->CONFIG = new Artisan_VO($root);
-	//}
-	
 	/**
 	 * Echos out the configuration as a print_r() with a pre wrapped around it.
 	 * @author vmc <vmc@leftnode.com>
