767f5e8beb15783c0714bba809b628e97cef7474
Vic Cherubini <vmc@leftnode.com>
2008-12-22 11:48:44 +0000

    Added Doxygen comments for getAsfw(), #3.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@549 c87a4359-4766-4260-b519-6004f3ce7042

--- Artisan/ASFW.php
+++ Artisan/ASFW.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * @see Artisan_System
+ */
+require_once 'Artisan/Artisan_System.php';
+
+/**
+ * Returns an instance of the Artisan_System class.
+ * @author vmc <vmc@leftnode.com>
+ * @retval Object Returns the only instance of the Artisan_System class.
+ */
 function &getAsfw() {
-	return &Artisan_System()::getInstance();
+	return &Artisan_System::getInstance();
 }
