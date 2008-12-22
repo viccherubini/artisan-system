6c5f50f5e12ff28756c144a8f02008f474841c69
Vic Cherubini <vmc@leftnode.com>
2008-12-22 03:45:33 +0000

    Added Doxygen comments for #3
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@530 c87a4359-4766-4260-b519-6004f3ce7042

--- Artisan/Registry.php
+++ Artisan/Registry.php
@@ -2,9 +2,22 @@
 
 require_once 'Artisan/Functions/Array.php';
 
+/**
+ * A registry class that holds data in a global scope. This class is static.
+ * @author vmc <vmc@leftnode.com>
+ */
 class Artisan_Registry {
+	///< A list of objects or variables to store.
 	private static $obj_list = array();
 
+	/**
+	 * Push a new object or variable onto the registry stack.
+	 * @author vmc <vmc@leftnode.com>
+	 * @param $name The name of variable to push, does not have to be the same as the variable name.
+	 * @param $obj The object or variable to push.
+	 * @param $force Force the variable onto the stack even if it already exists.
+	 * @retval boolean Returns true.
+	 */
 	public static function push($name, $obj, $force = false) {
 		$name = trim($name);
 		
@@ -12,8 +25,17 @@ class Artisan_Registry {
 		if ( (true === $found && true === $force) || false === $found ) {
 			self::$obj_list[$name] = $obj;
 		}
+		return true;
 	}
 	
+	/**
+	 * Pops an element from the registry stack. Note, this does not remove the object
+	 * unless explicitly called to.
+	 * @author vmc <vmc@leftnode.com>
+	 * @param $name The name of variable to pop.
+	 * @param $remove If true, removes the element from the stack.
+	 * @retval mixed Returns the object if found, NULL otherwise.
+	 */
 	public static function pop($name, $remove = false) {
 		$ret = NULL;
 		if ( true === asfw_exists($name, self::$obj_list) ) {
@@ -23,7 +45,6 @@ class Artisan_Registry {
 				unset(self::$obj_list[$name]);
 			}
 		}
-		
 		return $ret;
 	}
 }
