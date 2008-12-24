34de2c23013b1768b0080a2d9ddcb598e98d364b
Vic Cherubini <vmc@leftnode.com>
2008-12-24 13:45:51 +0000

    Initial checkin of RSS classes.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@554 c87a4359-4766-4260-b519-6004f3ce7042

---
+++ Artisan/Rss/Exception.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @see Artisan_Exception
+ */
+require_once 'Artisan/Exception.php';
+
+/**
+ * RSS Exception Class.
+ * @author vmc <vmc@leftnode.com>
+ */
+class Artisan_Rss_Exception extends Artisan_Exception {
+}
