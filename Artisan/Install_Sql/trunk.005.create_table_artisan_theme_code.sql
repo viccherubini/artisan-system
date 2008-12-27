8ecca19c7da7e5377544e4a7268ab3516dcce5ff
Vic Cherubini <vmc@leftnode.com>
2008-12-27 13:42:08 +0000

    Moved Install_Sql to the directory above
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@566 c87a4359-4766-4260-b519-6004f3ce7042

--- Artisan/Install_Sql/trunk.005.create_table_artisan_theme_code.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `artisan_theme_code` (
-	`theme_code_id` INT( 10 ) NOT NULL AUTO_INCREMENT ,
-	`theme_id` INT( 10 ) NOT NULL ,
-	`code_name` VARCHAR( 128 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL ,
-	`code` TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL ,
-	PRIMARY KEY ( `theme_code_id` )
-) ENGINE = MYISAM CHARACTER SET utf8 COLLATE utf8_unicode_ci;
