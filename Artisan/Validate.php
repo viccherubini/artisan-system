313c53b7eb7c464d23d81de943b2ed544618b109
Vic Cherubini <vmc@leftnode.com>
2008-12-27 14:13:09 +0000

    Added methods isAscii(), isUri(), isIpv4() , and isEmail() to this class as static methods to validate basic things. More methods to come.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@570 c87a4359-4766-4260-b519-6004f3ce7042

--- Artisan/Validate.php
+++ Artisan/Validate.php
@@ -3,7 +3,59 @@
 /**
  * Static class that contains methods to validate data.
  * @author vmc <vmc@leftnode.com>
+ * @todo Finish writing this class!
  */
 class Artisan_Validate {
 
+	public static function isAscii($val) {
+		$clamp_low = ord(' ');
+		$clamp_high = ord('~');
+	
+		$len = strlen($val);
+		$is_ascii = true;
+		for ( $i=0; $i<$len; $i++ ) {
+			if ( ord($val[$i]) < $clamp_low || ord($val[$i]) > $clamp_high ) {
+				$is_ascii = false;
+			}
+		}
+		return $is_ascii;
+	}
+	
+	public static function isUri($u) {
+		/**
+		 * $matches will look like:
+		 * $matches => Array(
+		 *  [0] => $u,
+		 *  [1] => http:// (also supports svn+ssh:// or other protocols)
+		 *  [2] => username:password@
+		 *  [3] => (www.|subdomain.)example.com -- If there is a subdomain or www., it is included here
+		 *  [4] => :443
+		 *  [5] => /index.php?query_string#anchor
+		 * );
+		 * I'm kinda proud of that RegEx :)
+		 */
+		$matches = array();
+		$match_uri = preg_match('#^([a-z\-\+]+://)?([a-z0-9]+:[a-z0-9]+\@){0,1}([a-z0-9.]*[a-z0-9-]+\.[a-z]+){1}(:[0-9]{1,5}){0,1}(.*)#i', $u, $matches);
+		
+		echo $match_uri;
+		asfw_print_r($matches);
+	}
+	
+	public static function isIpv4($ip) {
+		
+	}
+	
+	/**
+	 * Validates whether an email is valid or not. Does not fully meet requirements set forth by 
+	 * RFC 822 and RFC 2822. We're intentionally strict, however this lets a good amount through.
+	 * @author vmc <vmc@leftnode.com>
+	 * @param $s The email address to test.
+	 * @retval boolean True if the value is an email address, false otherwise.
+	 */
+	public static function isEmail($s) {
+		if ( 1 === preg_match('/([a-z0-9-_.!#$%^&*~`]*)(@[a-z0-9-]*\.[a-z]+)/i', $s) ) {
+			return true;
+		}
+		return false;
+	}
 }
