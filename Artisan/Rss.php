97ad1bf30a0c8824cea5efafe1e9ce0c398cc899
Vic Cherubini <vmc@leftnode.com>
2008-12-28 02:06:21 +0000

    Added Doxygen comments to the RSS feeder for #3
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@593 c87a4359-4766-4260-b519-6004f3ce7042

--- Artisan/Rss.php
+++ Artisan/Rss.php
@@ -9,20 +9,40 @@ require_once 'Artisan/Rss/Exception.php';
  * @see Artisan_Xml
  */
 require_once 'Artisan/Xml.php';
-require_once 'Artisan/Functions/Array.php';
 
+/**
+ * Creates an RSS feed by creating the XML.
+ * @author vmc <vmc@leftnode.com>
+ */
 abstract class Artisan_Rss {
+	///< The title of the channel.
 	private $_channel_title;
-	private $_channel_link;
-	private $_channel_description;
-	private $_channel_language = 'en-us';
-	private $_channel_generator;
 	
+	///< The link (URL) to the channel.
+	private $_channel_link;
+	
+	///< The description of the channel.
+	private $_channel_description;
+	
+	///< The language of the channel, defaults to 'en-us'.
+	private $_channel_language = 'en-us';
+	
+	///< The generator of the channel. Defaults to 'Artisan System Framework'.
+	private $_channel_generator = 'Artisan System Framework';
+	
+	///< An array of items to print in the RSS.
 	protected $_item_list = array();
 	
+	/**
+	 * Sets the configuration data which must contain the title, link, and description.
+	 * @author vmc <vmc@leftnode.com>
+	 * @param $CONFIG An instance of the Artisan_Config class.
+	 * @throw Artisan_Rss_Exception If the title, link, or description are not present in the $CONFIG.
+	 * @retval boolean Returns true.
+	 */
 	public function setConfig(Artisan_Config &$CONFIG) {
-		if ( false === $CONFIG->exists('title', 'link', 'description', 'generator') ) {
-			throw new Artisan_Rss_Exception(ARTISAN_WARNING, 'The config does not require the necessary properties (title, link, description, and generator).', __CLASS__, __FUNCTION__);
+		if ( false === $CONFIG->exists('title', 'link', 'description') ) {
+			throw new Artisan_Rss_Exception(ARTISAN_WARNING, 'The config does not require the necessary properties (title, link, and description).', __CLASS__, __FUNCTION__);
 		}
 		
 		$this->_channel_title = htmlentities($CONFIG->title);
@@ -31,34 +51,47 @@ abstract class Artisan_Rss {
 		if ( false === empty($CONFIG->language) ) {
 			$this->_channel_language = trim($CONFIG->language);
 		}
-		$this->_channel_generator = htmlentities($CONFIG->generator);
-		
+		if ( false === empty($CONFIG->generator) ) {
+			$this->_channel_generator = htmlentities($CONFIG->generator);
+		}
 		return true;
 	}
 	
-	
-	
+	/**
+	 * Adds an item to the RSS feed.
+	 * @author vmc <vmc@leftnode.com>
+	 * @param $ITEM A Value Object representation of the item.
+	 * @retval boolean True if the item is added, false if the title, link, description, pubDate, or author is not present in the $ITEM.
+	 * @todo Finish writing this function!
+	 */
 	public function addItem(Artisan_Vo $ITEM) {
+		exit('Finish implementing ' . __CLASS__ . '::' . __FUNCTION__);
+		
 		if ( false === $ITEM->exists('title', 'link', 'description', 'pubDate', 'author') ) {
 			return false;
 		}
 		
-		//$this->_item_list = array('item' => $ITEM->toArray());
 		$this->_item_list[] = $ITEM->toArray();
 	}
 	
-	
-	
+	/**
+	 * Sets the mapping between the source and the layout of the RSS feed.
+	 * @author vmc <vmc@leftnode.com>
+	 * @param $map The mapping array to map the data source and the RSS feed format.
+	 * @todo Finish writing this function!
+	 */
 	public function setMap($map) {
-		/*
-		this array corresponds to the database tables.
-		
-		*/
+		exit('Finish implementing ' . __CLASS__ . '::' . __FUNCTION__);
 	}
 	
-	abstract public function load();
-	
+	/**
+	 * Writes the feed data out to the RSS XML.
+	 * @author vmc <vmc@leftnode.com>
+	 * @retval string Returns the XML string.
+	 */
 	public function write() {
+		exit('Finish implementing ' . __CLASS__ . '::' . __FUNCTION__);
+		
 		$header = array(
 			'channel' => array(
 				'title' => $this->_channel_title,
@@ -69,14 +102,12 @@ abstract class Artisan_Rss {
 				'item' => $this->_item_list
 			)
 		);
-		
-		asfw_print_r($header);
-		
-		$header_xml = Artisan_Xml::toXml($header);
-		asfw_print_r($header_xml);
-		
-		//$item_xml = Artisan_Xml::toXml($this->_item_list, NULL);
-		
-		
 	}
+	
+	/**
+	 * Loads up the data from the specified source.
+	 * @author vmc <vmc@leftnode.com>
+	 * @retval boolean Returns true if successful load, false otherwise.
+	 */
+	abstract public function load();
 }
--- Artisan/Rss/Db.php
+++ Artisan/Rss/Db.php
@@ -1,14 +1,33 @@
 <?php
 
+/**
+ * @see Artisan_Rss
+ */
 require_once 'Artisan/Rss.php';
 
+/**
+ * Loads data for an RSS feed from a database.
+ * @author vmc <vmc@leftnode.com>
+ */
 class Artisan_Rss_Db extends Artisan_Rss {
+	///< The Database instance, must have an active connection.
 	protected $DB = NULL;
 	
+	/**
+	 * Builds a new object to load in RSS data from a database.
+	 * @author vmc <vmc@leftnode.com>
+	 * @param $DB The Database instance, must have an active connection.
+	 * @retval Object A new Artisan_Rss_Db object.
+	 */
 	public function __construct(Artisan_Db &$DB) {
 		$this->DB = &$DB;
 	}
 	
+	/**
+	 * Loads up the RSS data from the database.
+	 * @author vmc <vmc@leftnode.com>
+	 * @retval boolean Returns true.
+	 */
 	public function load() {
 	
 	}
