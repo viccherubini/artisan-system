c0813cc1e5b9def5df488fbcbc5aa8ff8d549cac
Vic Cherubini <vmc@leftnode.com>
2008-12-28 04:19:20 +0000

    Added helpful comments to index.php
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@599 c87a4359-4766-4260-b519-6004f3ce7042

--- httpdocs/index.php
+++ httpdocs/index.php
@@ -1,10 +1,21 @@
 <?php
 
+/**
+ * Artisan System Framework SDK 0.2
+ * This file is the entry point for the site.
+ */
+
+// Set's the include path to be up a directory so that Artisan/ can be loaded in.
+// This can be updated to be: set_include_path('.:/path/to/artisan/'); if you wish
+// to hardcode the path.
 set_include_path('.:..');
+
 
 require_once 'Artisan/Config/Array.php';
 require_once 'Artisan/Controller.php';
 
+// Create a new configuration instance for managing the controller. The
+// three keys provided are required.
 $config_controller = new Artisan_Config_Array(array(
 	'directory' => 'Controllers',
 	'default_controller' => 'Artisan',
@@ -12,7 +23,7 @@ $config_controller = new Artisan_Config_Array(array(
 	)
 );
 
-
+// The Artisan_Controller class is a singleton, so load it in as so.
 $C = &Artisan_Controller::get();
 $C->setConfig($config_controller);
 
