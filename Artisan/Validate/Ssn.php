59f89cbcd803b9d9e52a53d05d30cee2fd10ac3a
Vic Cherubini <vmc@leftnode.com>
2008-12-31 03:30:13 +0000

    Base SSN class
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@608 c87a4359-4766-4260-b519-6004f3ce7042

---
+++ Artisan/Validate/Ssn.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @see Artisan_Validate
+ */
+
+class Artisan_Validate_Ssn extends Artisan_Validate {
+	private $_ssn = NULL;
+	public function __construct($ssn = NULL) {
+
+	}
+
+	public function isValid() {
+
+	}
+}
