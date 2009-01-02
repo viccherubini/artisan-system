b3132a718f7f954620b215928549cd835d9a739c
Rod Afshar <rafshar@gmail.com>
2009-01-02 21:25:21 +0000

    Converted to non-static class & tested.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@642 c87a4359-4766-4260-b519-6004f3ce7042

--- Artisan/Validate/Alphanum.php
+++ Artisan/Validate/Alphanum.php
@@ -6,23 +6,32 @@
 require_once 'Artisan/Validate.php';
 
 /**
- * @see Artisan_Validate_Exception
- */
-require_once 'Artisan/Validate/Exception.php';
-
-/**
- * Static class that contains methods to validate credit cart numbers.
+ * Static class that contains methods to validate alphanumeric strings.
  * @author rafshar <rafshar@gmail.com>
  */
 class Artisan_Validate_Alphanum extends Artisan_Validate {
+	private $_s = NULL;
+
+	public function __construct($s = NULL) {
+		$this->_s = trim($s);
+	}
+
 	/**
 	 * Validates whether a submitted string is comprised of letters and digits only.
 	 * @author rafshar <rafshar@gmail.com>
 	 * @param $s The string to test.
 	 * @retval boolean True if the value is all alphanumeric, false otherwise.
 	 */
-	public static function isValid($s) {
-		if ( 1 === preg_match("/[^A-Za-z0-9]/", $s) ) {
+	public function isValid($s) {
+		if ( true === empty($s) ) {
+			$s = $this->_s;
+		}
+		
+		if ( true === empty($s) ) {
+			return false;
+		}
+
+		if ( 1 === preg_match("/[^a-z0-9]/i", $s) ) {
 			return false;
 		}
 		return true;
