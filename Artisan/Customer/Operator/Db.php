1dc0439411535e397f711b448c29f18e91d8ea43
Vic Cherubini <vmc@leftnode.com>
2009-01-02 12:47:04 +0000

    Removed old comments, updated the class that Artisan_Customer_Operator_Db extends, it now extends Artisan_User_Db rather than Artisan_Customer.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@639 c87a4359-4766-4260-b519-6004f3ce7042

--- Artisan/Customer/Operator/Db.php
+++ Artisan/Customer/Operator/Db.php
@@ -4,15 +4,14 @@
  * @see Artisan_Customer
  */
 //require_once 'Artisan/Customer.php';
+require_once 'Artisan/User/Db.php';
 
+require_once 'Artisan/Customer/Interface.php';
 
-class Artisan_Customer_Operator_Db extends Artisan_Customer {
-	private $DB = NULL;
+class Artisan_Customer_Operator_Db extends Artisan_User_Db {
+	// no constructor, use default
 	
-	public function __construct(Artisan_Db &$DB) {
-		$this->DB = &$DB;
-	}
+	// overwrite _update() and _insert() to take history into account
+	// comment_history management
 	
-	
-	//public 
 }
