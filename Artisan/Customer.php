476af830d232b06660d5b74993c55820cb6be3e3
Vic Cherubini <vmc@leftnode.com>
2009-01-02 12:46:03 +0000

    Added some comments to help out with whats needed, added the main class Doxygen comment
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@637 c87a4359-4766-4260-b519-6004f3ce7042

--- Artisan/Customer.php
+++ Artisan/Customer.php
@@ -7,24 +7,31 @@ require_once 'Artisan/User.php';
 
 /**
  * This class allows for the management of customers as through an e-commerce
- * or other type of customer management interface.
+ * or other type of customer management interface. This class will be built and 
+ * extends nothing. After it's built, an operator needs to be built and added to this
+ * class. Because each operator extends an Artisan_User class, they all have 
+ * overridden methods for writing the user data.
  * @author <vmc@leftnode.com>
  */
-class Artisan_Customer extends Artisan_User {
+class Artisan_Customer {
 	private $OP;
 	
 	
 	private $_customer_id = 0;
 	
 	
-	public function __construct($customer_id = 0) {
-		
-	}
-	/*
-	public function write() { }
+	//public function __construct($customer_id = 0) {
+	//	
+	//}
 	
-	protected function _insert();
-	protected function _update();
-	protected function _load($customer_id);
-	*/
+	public function setOperator(Artisan_Customer_Operator &$OP) {
+	
+	}
+	
+	public function getCustomerId() {
+		return $this->_user_id;
+	}
+	
+	// all we need here are write();
+	// and load();
 }
