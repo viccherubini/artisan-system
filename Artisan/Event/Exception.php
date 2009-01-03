8a1af0b88c158d931857860e232252960092bd9b
Vic Cherubini <vmc@leftnode.com>
2009-01-03 16:04:48 +0000

    New structure for event management classes.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@645 c87a4359-4766-4260-b519-6004f3ce7042

---
+++ Artisan/Event/Exception.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @see Artisan_Exception
+ */
+require_once 'Artisan/Exception.php';
+
+/**
+ * Event Exception Class.
+ * @author vmc <vmc@leftnode.com>
+ */
+class Artisan_Event_Exception extends Artisan_Exception {
+}
