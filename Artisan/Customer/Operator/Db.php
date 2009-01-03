b31e76d8ace6b7d44b6eac2b6327202b5347850a
Vic Cherubini <vmc@leftnode.com>
2009-01-03 16:29:16 +0000

    Added the default constructor to create a new user class, this may change
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@648 c87a4359-4766-4260-b519-6004f3ce7042

--- Artisan/Customer/Operator/Db.php
+++ Artisan/Customer/Operator/Db.php
@@ -6,11 +6,16 @@
 //require_once 'Artisan/Customer.php';
 require_once 'Artisan/Customer/Operator.php';
 
+
+require_once 'Artisan/User/Db.php';
+
 class Artisan_Customer_Operator_Db extends Artisan_Operator {
 	private $DB = NULL;
+	private $USER = NULL;
 	
 	public function __construct(Artisan_Db &$DB) {
-		$this->DB = &$DB;
+		//$this->DB = &$DB;
+		$this->USER = new Artisan_User_Db($DB);
 	}
 	
 	
