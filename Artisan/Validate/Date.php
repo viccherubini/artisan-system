868581ffa130bb8c86cf524ac4475c2677f3196a
Rod Afshar <rafshar@gmail.com>
2009-01-04 00:41:20 +0000

    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@656 c87a4359-4766-4260-b519-6004f3ce7042

---
+++ Artisan/Validate/Date.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * @see Artisan_Validate
+ */
+require_once 'Artisan/Validate.php';
+
+/**
+ * Static class that contains methods to validate date values.
+ * @author rafshar <rafshar@gmail.com>
+ */
+class Artisan_Validate_Date extends Artisan_Validate {
+	private $_date = NULL;
+
+	public function __construct($date = NULL) {
+		$this->_date = trim($date);
+	}
+	
+	/**
+	 * Validates whether a submitted string is a date.
+	 * @author rafshar <rafshar@gmail.com>
+	 * @param $date The date to test.
+	 * @retval boolean True if the value is a date, false otherwise.
+	 */
+	public function isValid($date = NULL) {
+		
+		// Potential Date Values:
+		// yyyy-mm-dd hh:mm:ss
+		// mm-dd-yyyy
+		// dd-mm-yyyy
+		
+		if ( true === empty($date) ) {
+			$date = $this->_date;
+		}
+		
+		if ( true === empty($date) ) {
+			return false;
+		}
+
+		// Enter code here
+		
+		return true;
+	}	
+}
