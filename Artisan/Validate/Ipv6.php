90d93fbd28e7620641cc9a0d6159e7642c816d51
Rod Afshar <rafshar@gmail.com>
2009-01-04 00:41:46 +0000

    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@659 c87a4359-4766-4260-b519-6004f3ce7042

---
+++ Artisan/Validate/Ipv6.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * @see Artisan_Validate
+ */
+require_once 'Artisan/Validate.php';
+
+/**
+ * Static class that contains methods to validate Ipv6 addresses.
+  * @author vmc <vmc@leftnode.com>
+ */
+class Artisan_Validate_Ipv6 extends Artisan_Validate {
+	private $_ip = NULL;
+
+	public function __construct($ip = NULL) {
+		$this->_ip = trim($ip);
+	}
+	
+	/**
+	 * Determines if a value is a valid IPv6 address.
+	 * @author vmc <vmc@leftnode.com>
+	 * @param $ip The IP address to test.
+	 * @retval boolean Returns true if $ip is a valid IPv6 address, false otherwise.
+	 * @todo Finish implementing this method.
+	 */
+	public function isValid($ip = NULL) {
+		if ( true === empty($ip) ) {
+			$ip = $this->_ip;
+		}
+		
+		if ( true === empty($ip) ) {
+			return false;
+		}
+	}
+}
