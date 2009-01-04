05f45e5eab1917c3d64802ac5bbdcf35b5dbdcee
Rod Afshar <rafshar@gmail.com>
2009-01-04 00:41:30 +0000

    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@657 c87a4359-4766-4260-b519-6004f3ce7042

---
+++ Artisan/Validate/Uri.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * @see Artisan_Validate
+ */
+require_once 'Artisan/Validate.php';
+
+/**
+ * Static class that contains methods to validate a Uri.
+  * @author vmc <vmc@leftnode.com>
+ */
+class Artisan_Validate_Uri extends Artisan_Validate {
+	private $_u = NULL;
+
+	public function __construct($u = NULL) {
+		$this->_u = trim($u);
+	}
+	
+	/**
+	 * Test if a value is a valid URI or not.
+	 * @author vmc <vmc@leftnode.com>
+	 * @param $u The URI to test.
+	 * @retval boolean Returns true if value is an array, false otherwise.
+	 * @todo Finish implementing this method.
+	 */
+	public function isValid($u = NULL) {
+		/**
+		 * $matches will look like:
+		 * @code
+		 * $matches => Array(
+		 *  [0] => $u,
+		 *  [1] => http:// (also supports svn+ssh:// or other protocols)
+		 *  [2] => username:password@
+		 *  [3] => (www.|subdomain.)example.com -- If there is a subdomain or www., it is included here
+		 *  [4] => :443
+		 *  [5] => /index.php?query_string#anchor
+		 * );
+		 * @endcode
+		 * I'm kinda proud of that RegEx :)
+		 */
+		if ( true === empty($u) ) {
+			$u = $this->_u;
+		}
+		
+		if ( true === empty($u) ) {
+			return false;
+		}
+		
+		$matches = array();
+		$match_uri = preg_match('#^([a-z\-\+]+://)?([a-z0-9]+:[a-z0-9]+\@){0,1}([a-z0-9.]*[a-z0-9-]+\.[a-z]+){1}(:[0-9]{1,5}){0,1}(.*)#i', $u, $matches);
+		
+		echo $match_uri;
+		asfw_print_r($matches);
+	}
+}
