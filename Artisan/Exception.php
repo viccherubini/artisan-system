bc8d2510efa99f1390e25ec6e29d14000df21e35
Vic Cherubini <vmc@leftnode.com>
2009-11-15 12:45:50 +0000

    Much cleaned up, no more silly comments.
    git-svn-id: http://svn.leftnode.com/svn/artisan_system/trunk@1060 c87a4359-4766-4260-b519-6004f3ce7042

--- Artisan/Exception.php
+++ Artisan/Exception.php
@@ -2,24 +2,10 @@
 
 require_once 'Artisan/Function/Array.php';
 
-/**
- * Artisan_Exception is a child of PHP's internal class Exception. As a result, it is *NOT* built
- * by Artisan. Therefore, it does not contain the similar methods of other Artisan classes. 
- * Artisan has it's own Exception class to overcome some of the shortcomings of the built in PHP
- * Exception class. Allows for prettier
- * exception formatting.
- * @author vmc <vmc@leftnode.com>
- */
 class Artisan_Exception extends Exception {
-	///< The line number that the exception was thrown from.
-	private $_line_number;
-
-	///< The file name the error occured in.
-	private $_file_name;
+	private $_class = NULL;
 	
-	private $_class_name = NULL;
-	
-	private $_function_name = NULL;
+	private $_function = NULL;
 	
 	/**
 	 * Default constructor.
@@ -29,36 +15,7 @@ class Artisan_Exception extends Exception {
 	 * @retval Object Returns new Artisan_Exception object.
 	 */
 	public function __construct($error_message) {
-		parent::__construct($error_message, 400);
-		
-		$this->_line_number = parent::getLine();
-		$this->_file_name = parent::getFile();
-	}
-	
-	/**
-	 * Overloaded Exception::toString() method. Stylizes how the error string looks.
-	 * @author vmc <vmc@leftnode.com>
-	 * @retval string Returns the string with exception data.
-	 */
-	public function toString() {
-		$trace = parent::getTrace();
-		$trace = current($trace);
-
-		$error_class = NULL;
-		if ( true === asfw_exists('class', $trace) ) {
-			$this->_class_name = $trace['class'];
-			$error_class = $this->_class_name . $trace['type'];
-		}
-		
-		if ( true === asfw_exists('function', $trace) ) {
-			$this->_function_name = $trace['function'];
-			$error_class .= $this->_function_name . '() > ';
-		}
-		
-		$error_file = $this->_file_name . ' +' . $this->_line_number;
-		$error_code = stripslashes($this->message) . ' (' . $error_file . ', Code: ' . $this->code . ')';
-		
-		return $error_class . $error_code;
+		parent::__construct($error_message);
 	}
 	
 	/**
@@ -67,42 +24,23 @@ class Artisan_Exception extends Exception {
 	 * @retval string Returns the string with exception data.
 	 */
 	public function __toString() {
-		return $this->toString();
-	}
-	
-	/**
-	 * Returns the file name the error occurred in.
-	 * @author vmc <vmc@leftnode.com>
-	 * @retval string The name of the file the error occurred in.
-	 */
-	public function getFileName() {
-		return $this->_file_name;
-	}
-	
-	/**
-	 * Returns the line number the error occurred on.
-	 * @author vmc <vmc@leftnode.com>
-	 * @retval integer The line number the error occured on.
-	 */
-	public function getLineNumber() {
-		return $this->_line_number;
-	}
-	
-	/**
-	 * Returns the class name the error occurred in.
-	 * @author vmc <vmc@leftnode.com>
-	 * @retval string The name of the class the error occurred in.
-	 */
-	public function getClassName() {
-		return $this->_class_name;
-	}
-	
-	/**
-	 * Returns the method name the error occurred in.
-	 * @author vmc <vmc@leftnode.com>
-	 * @retval string The name of the method the error occurred in.
-	 */
-	public function getFunctionName() {
-		return $this->_function_name;
+		$trace = parent::getTrace();
+		$trace = current($trace);
+
+		$error_class = NULL;
+		if ( true === isset($trace['class']) ) {
+			$this->_class = $trace['class'];
+			$error_class = $this->_class . $trace['type'];
+		}
+		
+		if ( true === isset($trace['function']) ) {
+			$this->_function = $trace['function'];
+			$error_class .= $this->_function . '() > ';
+		}
+		
+		$error_file = parent::getFile() . ' +' . parent::getLine();
+		$error_code = $this->message . ' (' . $error_file . ')';
+		
+		return $error_class . $error_code;
 	}
 }
